57579347bb581119b8041903dbee7752fc51b7dd
yundaehyuck <dbseogur2000@naver.com>
2023-04-17 16:01:36 +0900

    feat: create automatic chatgpt review algorithm api
    사용자가 키워드를 입력하면, chatgpt에게 보낼 query를 자동으로 구성하고,
    -
    완성된 query를 chatgpt에 보내 응답을 받는 api 작성
    S08P31C201-326

--- ai/main.py
+++ ai/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import os
+import openai
 
 app = FastAPI()
+
+#open api key
+openai.api_key = "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"
 
 @app.get("/")
 async def root():
@@ -11,3 +16,37 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message":f"Hello {name}"}
+
+
+@app.post("/review/gpt")
+async def create_review(title:str, words: str, writer=None, char=None):
+    
+    # message 구성
+    if writer != None and char != None:
+        
+        #default number of character value
+        char = max(80,char)
+            
+        m = f"제목:{title}, 키워드:{words}, 작가:{writer}, 서평 {char}자 이내"
+    
+    elif writer == None:
+        
+        #default number of character value
+        char = max(80,char)
+        
+        m = f"제목:{title}, 키워드:{words}, 서평 {char}자 이내"
+    
+    elif char == None:
+        
+        m = f"제목:{title}, 키워드:{words}, 작가:{writer}, 서평 80자 이내"
+    
+    #chatgpt request
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": m}
+    ]
+    )
+    
+    #chatgpt response
+    return completion.choices[0].message
