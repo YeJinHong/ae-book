1d50e431d22ecb0bc96aed474a40b7d202e15f34
문희주 <m5nacar5n@gmail.com>
2023-04-18 17:59:32 +0900

    feat: add saveReview(수정필요)
    isbn 받는 거 처리 필요
    S08P31C201-340

--- backend/src/main/java/com/c201/book/api/controller/ReviewController.java
+++ backend/src/main/java/com/c201/book/api/controller/ReviewController.java
@@ -2,19 +2,20 @@ package com.c201.book.api.controller;
 
 import com.c201.book.api.common.BaseResponse;
 import com.c201.book.api.request.ReviewReqDto;
+import com.c201.book.auth.CustomUserDetails;
+import com.c201.book.model.Review;
+import com.c201.book.service.ReviewServiceImpl;
 import com.c201.book.utils.DtoValidationUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * 서평 관련 컨트롤러
+ * [문희주] 서평 관련 컨트롤러
  */
 @Tag(name = "서평 Controller")
 @Slf4j
@@ -24,18 +25,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
 
     private final DtoValidationUtils dtoValidationUtils;
+    private final ReviewServiceImpl reviewService;
 
+    // 로그인 완성되면 주석 풀고 사용
+//    @Operation(summary = "서평 등록", description = "새 서평을 등록합니다.")
+//    @PostMapping(
+//            path = "/books/{isbn}",
+//            consumes = MediaType.APPLICATION_JSON_VALUE
+//    )
+//    public BaseResponse<?> saveReview(
+//            @RequestBody ReviewReqDto reviewReqDto,
+//            @AuthenticationPrincipal CustomUserDetails customUserDetails // 시큐리티 완성되면 주석 푸쇼 !
+//    ) {
+//        // DTO NOT NULL 검증
+//        dtoValidationUtils.validateReviewReqDto(reviewReqDto);
+//
+//        // 해당 책에 서평을 작성한 적 없는지 검증
+//        Review review = reviewService.findReviewByUserIdAndBookId(Long.parseLong(customUserDetails.getUsername()), reviewReqDto.getBookId());
+////        if(review != null) throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
+//
+//        // 서평 등록
+//        reviewService.saveReview(reviewReqDto);
+//
+//        return new BaseResponse<>(null, 200, "서평 작성 완료");
+//    }
+    
+    // 로그인 없이 DB로 테스트
     @Operation(summary = "서평 등록", description = "새 서평을 등록합니다.")
     @PostMapping(
             path = "/books/{isbn}",
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public BaseResponse<?> saveReview(
-            @RequestBody ReviewReqDto reviewReqDto
+            @RequestBody ReviewReqDto reviewReqDto,
+            @PathVariable String isbn
     ) {
         // DTO NOT NULL 검증
         dtoValidationUtils.validateReviewReqDto(reviewReqDto);
-
-        return null;
+    
+        // 해당 책에 서평을 작성한 적 없는지 검증
+        Review review = reviewService.findReviewByUserIdAndBookId(Long.parseLong(customUserDetails.getUsername()), reviewReqDto.getBookId());
+    //        if(review != null) throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
+    
+        // 서평 등록
+        reviewService.saveReview(reviewReqDto);
+    
+        return new BaseResponse<>(null, 200, "서평 작성 완료");
     }
 }
