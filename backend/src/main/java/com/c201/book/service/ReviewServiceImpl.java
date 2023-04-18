cd1b11f4367c72670199167893db55df9f085146
문희주 <m5nacar5n@gmail.com>
2023-04-18 16:27:37 +0900

    feat: create review controller
    1. 리뷰 컨트롤러 생성-2.swagger 연결 확인
    S08P31C201-340

--- backend/src/main/java/com/c201/book/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/c201/book/service/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.c201.book.service;
 
+import com.c201.book.api.common.BaseResponse;
+import com.c201.book.api.request.ReviewReqDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService{
 
+    /**
+     * [문희주] Review에 새로운 서평 추가
+     * @param reviewReqDto
+     * @return
+     */
+    @Override
+    public BaseResponse<?> saveReview(ReviewReqDto reviewReqDto) {
+        return null;
+    }
 }
