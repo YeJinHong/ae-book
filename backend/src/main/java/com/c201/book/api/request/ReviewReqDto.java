edbda40e77c3ed2d87103c403273c93ed3ad2df6
문희주 <m5nacar5n@gmail.com>
2023-04-18 17:55:21 +0900

    feat: add ReviewReqDto swagger description
    ReviewReqDto Swagger 설명과 예제 추가
    S08P31C201-340

--- backend/src/main/java/com/c201/book/api/request/ReviewReqDto.java
+++ backend/src/main/java/com/c201/book/api/request/ReviewReqDto.java
@@ -1,5 +1,6 @@
 package com.c201.book.api.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
@@ -9,7 +10,10 @@ import lombok.Data;
 
 @Data
 public class ReviewReqDto {
+    @Schema(description = "서평 내용", defaultValue = "장관이에요 절경이고요")
     private String content;
+    @Schema(description = "별점", defaultValue = "5")
     private int score;
+    @Schema(description = "도서 고유 아이디", defaultValue = "1")
     private long bookId;
 }
