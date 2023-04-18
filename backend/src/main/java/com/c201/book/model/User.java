21f40ea9dd02555910493c086953218aec0559b3
문희주 <m5nacar5n@gmail.com>
2023-04-18 10:57:21 +0900

    feat: set User Entity PK
    다른 Entity와 연관관계 설정을 위해 User Entity 임시 설정했습니다.
    어노테이션 추가, PK 설정
    S08P31C201-426

--- backend/src/main/java/com/c201/book/model/User.java
+++ backend/src/main/java/com/c201/book/model/User.java
@@ -1,4 +1,18 @@
 package com.c201.book.model;
 
-public class User {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
