96fada73a02ca64cecaabfc456ca4673ebfc580d
한선영 <gyeoul98@naver.com>
2023-04-18 13:37:43 +0900

    feat: User entity 컬럼 추가
    User entity에 들어가는 컬럼 정보를 추가로 작성했습니다.
    S08P31C201-396

--- backend/src/main/java/com/c201/book/model/User.java
+++ backend/src/main/java/com/c201/book/model/User.java
@@ -1,5 +1,6 @@
 package com.c201.book.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,4 +14,28 @@ public class User extends BaseEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "kakao_id", nullable = false, length = 200)
+    private Long kakaoId;
+
+    @Column(name = "nickname", length = 100)
+    private String nickname;
+
+    @Column(name = "phone", length = 100)
+    private String phone;
+
+    @Column(name = "profile_url", length = 200)
+    private String profileUrl;
+
+    @Column(name = "status", nullable = false)
+    private Boolean status;
+
+    @Builder
+    public User(Long kakaoId, String nickname, String phone, String profileUrl, Boolean status) {
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.phone = phone;
+        this.profileUrl = profileUrl;
+        this.status = status;
+    }
 }
