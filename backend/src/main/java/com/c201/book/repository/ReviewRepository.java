cf4ba52648cdaf909d443cae417b7c17bb25d6d6
m5nacar5n <m5nacar5n@gmail.com>
2023-04-20 00:36:42 +0900

    feat: findByBookId(Long bookId, Pageble pageable)
    S08P31C201-342

--- backend/src/main/java/com/c201/book/repository/ReviewRepository.java
+++ backend/src/main/java/com/c201/book/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.c201.book.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,5 @@ import com.c201.book.model.Review;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 	public Review findByUserIdAndBookId(Long userId, Long bookId);
 
-	public List<Review> findAllByBookId(Long bookId);
+	public Page<Review> findByBookId(Long bookId, Pageable pageable);
 }
