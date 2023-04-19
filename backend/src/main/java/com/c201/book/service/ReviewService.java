c28227a49d072534ee0402e9f9570a37bb123c88
m5nacar5n <m5nacar5n@gmail.com>
2023-04-20 00:33:07 +0900

    feat: getBookReviewList
    파라미터로 Pageable 객체를 받고 Page<ReviewResDto>를 return
    S08P31C201-342

--- backend/src/main/java/com/c201/book/service/ReviewService.java
+++ backend/src/main/java/com/c201/book/service/ReviewService.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import com.c201.book.api.request.ReviewReqDto;
 import com.c201.book.api.response.ReviewResDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
 	// 1. saveReview : 서평 등록
 	public void saveReview(Long userId, String isbn, ReviewReqDto reviewReqDto);
 
 	// 2. getBookReviewList : 특정 책에 달린 서평 리스트
-	public List<ReviewResDto> getBookReviewList(String isbn, int pageNumber, int pageSize, String sort);
+	public Page<ReviewResDto> getBookReviewList(String isbn, Pageable pageable);
 
 	// 3. getMyReviewList : 현재 로그인한 유저가 등록한 서평 리스트
 
@@ -20,5 +22,4 @@ public interface ReviewService {
 
 	// 6. deleteReview : 특정 서평 삭제
 
-	// 7.
 }
