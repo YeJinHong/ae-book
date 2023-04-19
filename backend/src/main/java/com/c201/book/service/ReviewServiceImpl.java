12629b2a0ea29471ac706acea354f51f5ce2df1e
문희주 <m5nacar5n@gmail.com>
2023-04-19 15:04:27 +0900

    refact: @Transactional 이동
    코멘트 반영
    S08P31C201-340

--- backend/src/main/java/com/c201/book/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/c201/book/service/ReviewServiceImpl.java
@@ -1,6 +1,8 @@
 package com.c201.book.service;
 
-import com.c201.book.api.common.BaseResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.c201.book.api.request.ReviewReqDto;
 import com.c201.book.model.Book;
 import com.c201.book.model.Review;
@@ -10,42 +12,41 @@ import com.c201.book.repository.ReviewRepository;
 import com.c201.book.repository.UserRepository;
 import com.c201.book.utils.exeption.CustomException;
 import com.c201.book.utils.exeption.ErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
-public class ReviewServiceImpl implements ReviewService{
+public class ReviewServiceImpl implements ReviewService {
 
-    private final ReviewRepository reviewRepository;
-    private final UserRepository userRepository;
-    private final BookRepository bookRepository;
+	private final ReviewRepository reviewRepository;
+	private final UserRepository userRepository;
+	private final BookRepository bookRepository;
 
-    @Override
-    public void saveReview(Long userId, String isbn, ReviewReqDto reviewReqDto) {
-        // 1. 유효한 isbn인지 검증
-        Book book = bookRepository.findByIsbn(isbn).orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
+	@Override
+	@Transactional
+	public void saveReview(Long userId, String isbn, ReviewReqDto reviewReqDto) {
+		// 1. 유효한 isbn인지 검증
+		Book book = bookRepository.findByIsbn(isbn).orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
 
-        // 2. 유효한 userId인지 검증
-        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+		// 2. 유효한 userId인지 검증
+		User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        // 3. 해당 책에 서평을 작성한 적 없는지 검증
-        Review review = reviewRepository.findByUserIdAndBookId(userId, book.getId());
-        if(review != null) throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
+		// 3. 해당 책에 서평을 작성한 적 없는지 검증
+		Review review = reviewRepository.findByUserIdAndBookId(userId, book.getId());
+		if (review != null) {
+			throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
+		}
 
-        // 4. 서평 저장
-        reviewRepository.save(Review.builder()
-                        .content(reviewReqDto.getContent())
-                        .score(reviewReqDto.getScore())
-                        .user(user)
-                        .book(book)
-                        .build());
+		// 4. 서평 저장
+		reviewRepository.save(Review.builder()
+			.content(reviewReqDto.getContent())
+			.score(reviewReqDto.getScore())
+			.user(user)
+			.book(book)
+			.build());
 
-        // 5. 도서 별점 정보 갱신
-        book.updateScoreInfo(reviewReqDto.getScore());
-    }
+		// 5. 도서 별점 정보 갱신
+		book.updateScoreInfo(reviewReqDto.getScore());
+	}
 }
