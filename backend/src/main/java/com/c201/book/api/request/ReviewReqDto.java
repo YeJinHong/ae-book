8dd4a2a439284882727a774e563978bd40434439
문희주 <m5nacar5n@gmail.com>
2023-04-19 15:03:53 +0900

    refact: @Data 제거
    코멘트 반영
    S08P31C201-340

--- backend/src/main/java/com/c201/book/api/request/ReviewReqDto.java
+++ backend/src/main/java/com/c201/book/api/request/ReviewReqDto.java
@@ -1,17 +1,23 @@
 package com.c201.book.api.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * [ 문희주 ] 서평 등록 Dto
  * 내용, 별점
  */
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewReqDto {
-    @Schema(description = "서평 내용", defaultValue = "장관이에요 절경이고요")
-    private String content;
-    @Schema(description = "별점", defaultValue = "5")
-    private int score;
+	@Schema(description = "서평 내용", defaultValue = "장관이에요 절경이고요")
+	private String content;
+	@Schema(description = "별점", defaultValue = "5")
+	private int score;
 }
