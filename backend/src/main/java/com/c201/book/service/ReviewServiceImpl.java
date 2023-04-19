2dcbb9c20b53847c1b23f507156b47e03e9e946b
문희주 <m5nacar5n@gmail.com>
2023-04-19 14:14:31 +0900

    feat: add saveReview
    서평 등록 기능임니다 ^_^
    S08P31C201-340

--- backend/src/main/java/com/c201/book/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/c201/book/service/ReviewServiceImpl.java
@@ -2,11 +2,19 @@ package com.c201.book.service;
 
 import com.c201.book.api.common.BaseResponse;
 import com.c201.book.api.request.ReviewReqDto;
+import com.c201.book.model.Book;
 import com.c201.book.model.Review;
+import com.c201.book.model.User;
+import com.c201.book.repository.BookRepository;
 import com.c201.book.repository.ReviewRepository;
+import com.c201.book.repository.UserRepository;
+import com.c201.book.utils.exeption.CustomException;
+import com.c201.book.utils.exeption.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,14 +22,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewServiceImpl implements ReviewService{
 
     private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final BookRepository bookRepository;
 
     @Override
-    public BaseResponse<?> saveReview(ReviewReqDto reviewReqDto) {
-        return null;
-    }
+    public void saveReview(Long userId, String isbn, ReviewReqDto reviewReqDto) {
+        // 1. 유효한 isbn인지 검증
+        Book book = bookRepository.findByIsbn(isbn).orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
 
-    @Override
-    public Review findReviewByUserIdAndBookId(Long userId, Long bookId) {
-        return reviewRepository.findByUserIdAndBookId(userId, bookId);
+        // 2. 유효한 userId인지 검증
+        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        // 3. 해당 책에 서평을 작성한 적 없는지 검증
+        Review review = reviewRepository.findByUserIdAndBookId(userId, book.getId());
+        if(review != null) throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
+
+        // 4. 서평 저장
+        reviewRepository.save(Review.builder()
+                        .content(reviewReqDto.getContent())
+                        .score(reviewReqDto.getScore())
+                        .user(user)
+                        .book(book)
+                        .build());
+
+        // 5. 도서 별점 정보 갱신
+        book.updateScoreInfo(reviewReqDto.getScore());
     }
 }
