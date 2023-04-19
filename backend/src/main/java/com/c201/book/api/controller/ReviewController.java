8c55cc1390aa379de4002e57fb79d98ee812a5a3
문희주 <m5nacar5n@gmail.com>
2023-04-19 14:14:31 +0900

    feat: add saveReview
    서평 등록 기능임니다 ^_^
    S08P31C201-340

--- backend/src/main/java/com/c201/book/api/controller/ReviewController.java
+++ backend/src/main/java/com/c201/book/api/controller/ReviewController.java
@@ -4,6 +4,7 @@ import com.c201.book.api.common.BaseResponse;
 import com.c201.book.api.request.ReviewReqDto;
 import com.c201.book.auth.CustomUserDetails;
 import com.c201.book.model.Review;
+import com.c201.book.model.User;
 import com.c201.book.service.ReviewServiceImpl;
 import com.c201.book.utils.DtoValidationUtils;
 import io.swagger.v3.oas.annotations.Operation;
@@ -26,50 +27,29 @@ public class ReviewController {
 
     private final DtoValidationUtils dtoValidationUtils;
     private final ReviewServiceImpl reviewService;
+//    private final TokenUtils tokenUtils;
 
     // 로그인 완성되면 주석 풀고 사용
-//    @Operation(summary = "서평 등록", description = "새 서평을 등록합니다.")
-//    @PostMapping(
-//            path = "/books/{isbn}",
-//            consumes = MediaType.APPLICATION_JSON_VALUE
-//    )
-//    public BaseResponse<?> saveReview(
-//            @RequestBody ReviewReqDto reviewReqDto,
-//            @AuthenticationPrincipal CustomUserDetails customUserDetails // 시큐리티 완성되면 주석 푸쇼 !
-//    ) {
-//        // DTO NOT NULL 검증
-//        dtoValidationUtils.validateReviewReqDto(reviewReqDto);
-//
-//        // 해당 책에 서평을 작성한 적 없는지 검증
-//        Review review = reviewService.findReviewByUserIdAndBookId(Long.parseLong(customUserDetails.getUsername()), reviewReqDto.getBookId());
-////        if(review != null) throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
-//
-//        // 서평 등록
-//        reviewService.saveReview(reviewReqDto);
-//
-//        return new BaseResponse<>(null, 200, "서평 작성 완료");
-//    }
-    
-    // 로그인 없이 DB로 테스트
     @Operation(summary = "서평 등록", description = "새 서평을 등록합니다.")
     @PostMapping(
-            path = "/books/{isbn}",
+            path = "/{isbn}",
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public BaseResponse<?> saveReview(
-            @RequestBody ReviewReqDto reviewReqDto,
-            @PathVariable String isbn
+            @PathVariable String isbn,
+            @RequestBody ReviewReqDto reviewReqDto
+//            ,@AuthenticationPrincipal CustomUserDetails customUserDetails // 시큐리티 완성되면 주석 해제하고 쉼표 옮기기
     ) {
+        // 토큰 유효성 검증
+        // User loginUser = tokenUtils.validateGetUser(customUserDetails);
+
         // DTO NOT NULL 검증
         dtoValidationUtils.validateReviewReqDto(reviewReqDto);
-    
-        // 해당 책에 서평을 작성한 적 없는지 검증
-        Review review = reviewService.findReviewByUserIdAndBookId(Long.parseLong(customUserDetails.getUsername()), reviewReqDto.getBookId());
-    //        if(review != null) throw new CustomException(ErrorCode.DUPLICATED_REVIEW);
-    
+
         // 서평 등록
-        reviewService.saveReview(reviewReqDto);
-    
+//        reviewService.saveReview(Long.parseLong(customUserDetails.getUsername()), reviewReqDto); // 로그인 완성되면 사용
+        reviewService.saveReview(1L, isbn, reviewReqDto); // 로그인 완성 전 하드코딩
+
         return new BaseResponse<>(null, 200, "서평 작성 완료");
     }
 }
