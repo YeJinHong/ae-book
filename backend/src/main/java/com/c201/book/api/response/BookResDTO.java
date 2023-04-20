409195a167c52fb7083e23e6576cd79e78d6fe00
leedoyeon <ldy__0202@naver.com>
2023-04-20 09:05:38 +0900

    refact: DTO 제목 수정

--- backend/src/main/java/com/c201/book/api/response/BookResDTO.java
+++ backend/src/main/java/com/c201/book/api/response/BookResDTO.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import java.sql.Date;
 
 @Getter
-public class BookResDto {
+public class BookResDTO {
     private String isbn;
     private String title;
     private String author;
@@ -13,7 +13,7 @@ public class BookResDto {
     private Date publishDate;
     private int price;
     private String coverImageUrl;
-    private String aladinUrl
+    private String aladinUrl;
     private int page;
 
 }
