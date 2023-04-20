3cb72c72d2d45058fb8ddfa1b093e7af2bdeead9
leedoyeon <ldy__0202@naver.com>
2023-04-20 16:12:57 +0900

    refact: 패키지 변경 사항에 따라 코드 이동
    패키지 수정에 따라 기존에 작성한 코드를 옮겼습니다.
    S08P31C201-366

--- backend/src/main/java/com/c201/aebook/api/book/presentation/controller/BookController.java
+++ backend/src/main/java/com/c201/aebook/api/book/presentation/controller/BookController.java
@@ -1,4 +1,32 @@
 package com.c201.aebook.api.book.presentation.controller;
 
+import com.c201.aebook.api.book.persistence.entity.BookEntity;
+import com.c201.aebook.api.book.service.BookService;
+import com.c201.aebook.api.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "도서 Controller")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/books")
 public class BookController {
+    private final BookService bookService;
+
+    @Operation(summary = "ISBN으로 도서 검색", description = "해당 ISBN의 도서 상세 정보를 반환합니다.")
+    @GetMapping("/{isbn}")
+    public BaseResponse<BookEntity> searchBookDetail(@PathVariable String isbn){
+        BookEntity book = bookService.searchBookDetail(isbn);
+        return new BaseResponse<>(book, 200, "도서 상세 정보 조회 성공");
+    }
+    // TODO : 키워드 검색으로 도서 리스트를 반환하는 searchBookList
+
+    // TODO : 새로 들어온 책 리스트를 반환하는 getNewBookList()
+
+    // TODO : 검색어 자동완성 getAutocompleteTitle()
+
 }
--- backend/src/main/java/com/c201/aebook/api/book/service/impl/BookServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/book/service/impl/BookServiceImpl.java
@@ -1,7 +1,23 @@
 package com.c201.aebook.api.book.service.impl;
 
+import com.c201.aebook.api.book.persistence.entity.BookEntity;
+import com.c201.aebook.api.book.persistence.repository.BookRepository;
+import com.c201.aebook.api.book.service.BookService;
+import com.c201.aebook.utils.exception.CustomException;
+import com.c201.aebook.utils.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BookServiceImpl {
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public BookEntity searchBookDetail(String isbn) {
+        BookEntity book = bookRepository.findByIsbn(isbn).orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
+        return book;
+    }
 }
