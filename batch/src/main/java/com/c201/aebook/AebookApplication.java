0f317ceae34c1743d82ba9221c42c4e9f3e3df5a
이해민 <2haemin98@gmail.com>
2023-04-20 16:36:20 +0900

    refact : batch 프로젝트 생성
    batch 프로젝트 따로 프로젝트로 생성
    S08P31C201-289

---
+++ batch/src/main/java/com/c201/aebook/AebookApplication.java
@@ -0,0 +1,13 @@
+package com.c201.aebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AebookApplication.class, args);
+	}
+
+}
