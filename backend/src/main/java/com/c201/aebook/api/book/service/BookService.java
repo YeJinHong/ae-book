727d7c24bc3065078a417f4905b10a88d34dfd4f
leedoyeon <ldy__0202@naver.com>
2023-04-20 16:12:57 +0900

    refact: 패키지 변경 사항에 따라 코드 이동
    패키지 수정에 따라 기존에 작성한 코드를 옮겼습니다.
    S08P31C201-366

--- backend/src/main/java/com/c201/aebook/api/book/service/BookService.java
+++ backend/src/main/java/com/c201/aebook/api/book/service/BookService.java
@@ -1,4 +1,7 @@
 package com.c201.aebook.api.book.service;
 
+import com.c201.aebook.api.book.persistence.entity.BookEntity;
+
 public interface BookService {
+    public BookEntity searchBookDetail(String isbn);
 }
