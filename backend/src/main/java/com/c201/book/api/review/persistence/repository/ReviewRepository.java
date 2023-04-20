d64aa1cff059d21673fd2abc0a511e1377101a83
문희주 <m5nacar5n@gmail.com>
2023-04-20 10:50:18 +0900

    feat: getBookReviewList
    특정 도서 isbn으로 서평 리스트를 return
    S08P31C201-342

--- backend/src/main/java/com/c201/book/api/review/persistence/repository/ReviewRepository.java
+++ backend/src/main/java/com/c201/book/api/review/persistence/repository/ReviewRepository.java
@@ -1,5 +1,7 @@
 package com.c201.book.api.review.persistence.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.c201.book.api.review.persistence.entity.ReviewEntity;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-    public ReviewEntity findByUserIdAndBookId(Long userId, Long bookId);
+	public ReviewEntity findByUserIdAndBookId(Long userId, Long bookId);
+
+	public Page<ReviewEntity> findByBookId(Long bookId, Pageable pageable);
 }
--- backend/src/main/java/com/c201/book/api/review/service/ReviewService.java
+++ backend/src/main/java/com/c201/book/api/review/service/ReviewService.java
@@ -1,18 +1,17 @@
 package com.c201.book.api.review.service;
 
-import java.util.List;
-
-import com.c201.book.api.response.ReviewResDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
 import com.c201.book.api.review.presentation.dto.request.ReviewRequestDTO;
+import com.c201.book.api.review.presentation.dto.response.ReviewResponseDTO;
 
 public interface ReviewService {
-    // 1. saveReview : 서평 등록
-    public void saveReview(Long userId, String isbn, ReviewRequestDTO reviewReqDto);
+	// 1. saveReview : 서평 등록
+	public void saveReview(Long userId, String isbn, ReviewRequestDTO reviewReqDto);
 
 	// 2. getBookReviewList : 특정 책에 달린 서평 리스트
-	public Page<ReviewResDto> getBookReviewList(String isbn, Pageable pageable);
+	public Page<ReviewResponseDTO> getBookReviewList(String isbn, Pageable pageable);
 
 	// 3. getMyReviewList : 현재 로그인한 유저가 등록한 서평 리스트
 
