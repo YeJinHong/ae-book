f72dfbe94b56debdc28d89ebc9960c6e86a3812c
문희주 <m5nacar5n@gmail.com>
2023-04-20 17:35:43 +0900

    feat: add toReviewModifySO
    ReviewModifySO Converter 추가
    S08P31C201-343

--- backend/src/main/java/com/c201/aebook/converter/ReviewConverter.java
+++ backend/src/main/java/com/c201/aebook/converter/ReviewConverter.java
@@ -2,12 +2,16 @@ package com.c201.aebook.converter;
 
 import org.mapstruct.Mapper;
 
+import com.c201.aebook.api.review.presentation.dto.request.ReviewModifyRequestDTO;
 import com.c201.aebook.api.review.presentation.dto.request.ReviewRequestDTO;
+import com.c201.aebook.api.vo.ReviewModifySO;
 import com.c201.aebook.api.vo.ReviewSO;
 
 @Mapper(componentModel = "spring")
 public interface ReviewConverter {
 
 	ReviewSO toReviewSO(ReviewRequestDTO reviewRequestDTO);
+
+	ReviewModifySO toReviewModifySO(ReviewModifyRequestDTO reveiwModifyRequestDTO);
 
 }
