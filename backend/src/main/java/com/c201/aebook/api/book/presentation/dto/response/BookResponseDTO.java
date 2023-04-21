b56c43e3aab209fe9a88bf14237ca61014f2dad5
leedoyeon <ldy__0202@naver.com>
2023-04-21 17:47:50 +0900

    refact: 인용구 따옴표 수정
    S08P31C201-366
    
    
    Former-commit-id: e4ea516cd5f135700696e4babf0c6c53492738bd

--- backend/src/main/java/com/c201/aebook/api/book/presentation/dto/response/BookResponseDTO.java
+++ backend/src/main/java/com/c201/aebook/api/book/presentation/dto/response/BookResponseDTO.java
@@ -15,7 +15,7 @@ public class BookResponseDTO {
     @Schema(description = "도서 제목", defaultValue = "철학이 필요한 시간 - 강신주의 인문학 카운슬링")
     private String title;
 
-    @Schema(description = "도서 소개", defaultValue = "나는 왜 이러고 살지?”의 주인공들을 위한 인문 공감 에세이. 지금은 자기 위로와 자기 최면이 아닌 아파도 당당하게 상처를 마주할 수 있게 하는 인문학이 필요하다. 이 책은 니체, 스피노자, 원효, 데리다 등 철학자들의 인문 고전을 통해 고민과 불안에 갇혀 있는 이들에게 솔직하게 삶에 직면하고 상처를 치유할 수 있는 방법을 제시한다.")
+    @Schema(description = "도서 소개", defaultValue = "\"나는 왜 이러고 살지?\"의 주인공들을 위한 인문 공감 에세이. 지금은 자기 위로와 자기 최면이 아닌 아파도 당당하게 상처를 마주할 수 있게 하는 인문학이 필요하다. 이 책은 니체, 스피노자, 원효, 데리다 등 철학자들의 인문 고전을 통해 고민과 불안에 갇혀 있는 이들에게 솔직하게 삶에 직면하고 상처를 치유할 수 있는 방법을 제시한다.")
     private String description;
 
     @Schema(description = "지은이", defaultValue = "강신주 지음")
