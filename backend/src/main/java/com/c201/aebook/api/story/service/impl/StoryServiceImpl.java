d7eb10977fa0af795e97fc1310324ff9caf7846e
홍예진 <teepij@naver.com>
2023-04-23 19:46:48 +0900

    feat: S3 연결 및 동화 업로드 api 작성
    - aws S3 접속 정보 application-key.yml에 추가.
    - S3 통신 컴포넌트 S3Uploader.java 작성
    - 제목, 내용, 동화 이미지가 들어올 때 해당 내용을 DB와 S3에 저장하는
      saveStory 작성
    - DB Connection pool 문제를 해결하기 위한 application-db.yml 일부
      수정있음
    S08P31C201-335

--- backend/src/main/java/com/c201/aebook/api/story/service/impl/StoryServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/story/service/impl/StoryServiceImpl.java
@@ -1,4 +1,35 @@
 package com.c201.aebook.api.story.service.impl;
 
-public class StoryServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.c201.aebook.api.story.persistence.entity.StoryEntity;
+import com.c201.aebook.api.story.persistence.repository.StoryRepository;
+import com.c201.aebook.api.story.service.StoryService;
+import com.c201.aebook.api.user.persistence.entity.UserEntity;
+import com.c201.aebook.api.user.persistence.repository.UserRepository;
+import com.c201.aebook.api.vo.StorySO;
+import com.c201.aebook.utils.exception.CustomException;
+import com.c201.aebook.utils.exception.ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StoryServiceImpl implements StoryService {
+	private final StoryRepository storyRepository;
+	private final UserRepository userRepository;
+
+	@Override
+	public void saveStory(StorySO storySO) {
+		// 유효한 userId인지 검증
+		UserEntity user = userRepository.findById(storySO.getUserId())
+			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+		storyRepository.save(StoryEntity.builder()
+			.title(storySO.getTitle())
+			.content(storySO.getContent())
+			.imgUrl(storySO.getImgUrl())
+			.user(user)
+			.build());
+	}
 }
