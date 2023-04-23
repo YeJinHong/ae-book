a15bbe364d118483286f2c1b8945d7c6e69561c3
홍예진 <teepij@naver.com>
2023-04-23 19:46:48 +0900

    feat: S3 연결 및 동화 업로드 api 작성
    - aws S3 접속 정보 application-key.yml에 추가.
    - S3 통신 컴포넌트 S3Uploader.java 작성
    - 제목, 내용, 동화 이미지가 들어올 때 해당 내용을 DB와 S3에 저장하는
      saveStory 작성
    - DB Connection pool 문제를 해결하기 위한 application-db.yml 일부
      수정있음
    S08P31C201-335

--- backend/src/main/java/com/c201/aebook/api/story/persistence/repository/StoryRepository.java
+++ backend/src/main/java/com/c201/aebook/api/story/persistence/repository/StoryRepository.java
@@ -1,7 +1,11 @@
 package com.c201.aebook.api.story.persistence.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.c201.aebook.api.story.persistence.entity.StoryEntity;
+
 @Repository
-public interface StoryRepository {
+public interface StoryRepository extends JpaRepository<StoryEntity, Long> {
+
 }
