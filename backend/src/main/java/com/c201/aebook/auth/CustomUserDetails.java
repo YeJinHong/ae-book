237a7ea05110bc75fe89ac1b8c5b979d13b66ac0
한선영 <gyeoul98@naver.com>
2023-04-24 14:23:12 +0900

    comment: UserDetails 주석 변경
    UserDetails 상태를 확인하기 위해 log를 확인하고 주석으로 변경하였습니다.
    S08P31C201-400

--- backend/src/main/java/com/c201/aebook/auth/CustomUserDetails.java
+++ backend/src/main/java/com/c201/aebook/auth/CustomUserDetails.java
@@ -29,12 +29,12 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return String.valueOf(this.user.getId());
+        return String.valueOf(user.getId());
     }
 
     @Override
     public String getUsername() {
-        return String.valueOf(this.user.getId());
+        return String.valueOf(user.getId());
     }
 
     @Override
