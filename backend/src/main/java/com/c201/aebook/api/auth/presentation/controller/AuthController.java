87fb3972b2288914dd003cdc28993b4a9a41c965
한선영 <gyeoul98@naver.com>
2023-04-24 14:00:12 +0900

    feat: access token 재발행 기능 구현
    refresh token을 기반으로 access token을 재발행 하는 기능을 구현하였습니다.
    S08P31C201-400

--- backend/src/main/java/com/c201/aebook/api/auth/presentation/controller/AuthController.java
+++ backend/src/main/java/com/c201/aebook/api/auth/presentation/controller/AuthController.java
@@ -1,11 +1,17 @@
 package com.c201.aebook.api.auth.presentation.controller;
 
+import com.c201.aebook.api.auth.service.impl.AuthServiceImpl;
+import com.c201.aebook.auth.CustomUserDetails;
+import com.c201.aebook.config.jwt.JwtProperties;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import com.c201.aebook.api.common.LoginUserInfoDTO;
 import com.c201.aebook.api.common.TokenDTO;
@@ -19,6 +25,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Tag(name="회원인증")
 @Slf4j
 @RequiredArgsConstructor
@@ -26,23 +35,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthServiceImpl authService;
+    private final RedisTemplate redisTemplate;
 
     @Operation(summary = "카카오 소셜 로그인", description = "카카오로 소셜 로그인을 합니다.")
     @GetMapping("/login")
     public ResponseEntity<?> login(
             @RequestParam(name = "code") String code
     ) {
-        // log.info("code : {} ", code);
+        log.info("code : {} ", code);
 
         // 1. 프론트에서 받은 kakao 인가 코드를 통해 kakao accessToken 발급
         KakaoTokenDTO kakaoTokenDto = authService.getAccessToken(code);
-        // log.info("KakaoTokenDto : {} ", kakaoTokenDto);
+        log.info("KakaoTokenDto : {} ", kakaoTokenDto);
 
         // 2. 발급 받은 accessToken으로 카카오 회원 정보 확인 후 DB 저장 또는 로그인(토큰 발급)
         LoginUserInfoDTO loginUserInfoDto = authService.saveUserOrLogin(kakaoTokenDto.getAccess_token());
-        // log.info("access Token : {} ", loginUserInfoDto.getTokenDto().getAccessToken());
-        // log.info("user name : {}", loginUserInfoDto.getUser().getNickname());
+        log.info("access Token : {} ", loginUserInfoDto.getTokenDto().getAccessToken());
+        log.info("user name : {}", loginUserInfoDto.getUser().getNickname());
 
         // 3. 헤더에 사용자 token 정보 담기
         TokenDTO tokenDto = loginUserInfoDto.getTokenDto();
@@ -51,7 +61,7 @@ public class AuthController {
                 tokenDto.getGrantType() + " " + tokenDto.getAccessToken());
         headers.add(tokenDto.getRefreshHeader(),
                 tokenDto.getGrantType() + " " + tokenDto.getRefreshToken());
-        // log.info("headers : {} ", headers);
+        log.info("headers : {} ", headers);
 
         // 4. 로그인 유저 정보 담기
         UserEntity user = loginUserInfoDto.getUser();
@@ -62,6 +72,32 @@ public class AuthController {
                 .build();
 
         return ResponseEntity.ok().headers(headers).body(loginResDto);
+    }
+
+    @Operation(summary = "토큰 재발행", description = "토큰 재발행을 합니다.")
+    @PostMapping(path = "/access-token")
+    public ResponseEntity<?> reissueAccessToken(
+            HttpServletResponse response,
+            HttpServletRequest request
+    ) {
+        // 받은 token 정보 가져오기
+        String jwt = authService.resolveToken(request, JwtProperties.AUTHORIZATION_HEADER);
+        String refresh = authService.resolveToken(request, JwtProperties.REFRESH_HEADER);
+        // log.info("jwt : {} ", jwt);
+        // log.info("refresh : {}", refresh);
+
+        // requestTokenDto에 받은 토큰 정보 저장하기
+        TokenDTO requestTokenDto = TokenDTO.builder()
+                .accessToken(jwt)
+                .refreshToken(refresh).build();
+
+        // 토큰 재발행
+        TokenDTO tokenDto = authService.reissueAccessToken(requestTokenDto);
+
+        // 헤더에 토큰 정보 담기
+        response.setHeader(JwtProperties.AUTHORIZATION_HEADER, tokenDto.getGrantType() + " " + tokenDto.getAccessToken());
+        response.setHeader(JwtProperties.REFRESH_HEADER, tokenDto.getGrantType() + " " + tokenDto.getRefreshToken());
+        return new ResponseEntity<>("access token 재발행 성공", HttpStatus.OK);
     }
 
 }
