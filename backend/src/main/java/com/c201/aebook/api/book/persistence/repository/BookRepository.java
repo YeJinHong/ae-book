5fc45d28e81676e3dcacff3d91f10a9b66b25ba6
leedoyeon <ldy__0202@naver.com>
2023-04-24 11:49:17 +0900

    feat: pagination 적용
    S08P31C201-369
    
    
    Former-commit-id: 3b7c9f7b836a82d247b659e575354ef52a36865b

--- backend/src/main/java/com/c201/aebook/api/book/persistence/repository/BookRepository.java
+++ backend/src/main/java/com/c201/aebook/api/book/persistence/repository/BookRepository.java
@@ -1,16 +1,20 @@
 package com.c201.aebook.api.book.persistence.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.c201.aebook.api.book.persistence.entity.BookEntity;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
-    public Optional<BookEntity> findByIsbn(String isbn);
-    public List<BookEntity> findTop5ByTitleContaining(String keyword);
-    public List<BookEntity> findByTitleContaining(String keyword);
+	public Optional<BookEntity> findByIsbn(String isbn);
+
+	public List<BookEntity> findTop5ByTitleContaining(String keyword);
+
+	public Page<BookEntity> findByTitleContaining(String keyword, Pageable pageable);
 }
--- backend/src/main/java/com/c201/aebook/api/book/service/impl/BookServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/book/service/impl/BookServiceImpl.java
@@ -1,5 +1,12 @@
 package com.c201.aebook.api.book.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.c201.aebook.api.book.persistence.entity.BookEntity;
 import com.c201.aebook.api.book.persistence.repository.BookRepository;
 import com.c201.aebook.api.book.presentation.dto.response.BookResponseDTO;
@@ -7,61 +14,56 @@ import com.c201.aebook.api.book.presentation.dto.response.BookSearchResponseDTO;
 import com.c201.aebook.api.book.service.BookService;
 import com.c201.aebook.utils.exception.CustomException;
 import com.c201.aebook.utils.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    private final BookRepository bookRepository;
+	private final BookRepository bookRepository;
 
-    @Override
-    public BookResponseDTO searchBookDetail(String isbn) {
-        BookEntity book = bookRepository.findByIsbn(isbn).orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
-        BookResponseDTO bookResponseDTO = BookResponseDTO.builder()
-                .aladinUrl(book.getAladinUrl())
-                .price(book.getPrice())
-                .author(book.getAuthor())
-                .coverImageUrl(book.getCoverImageUrl())
-                .title(book.getTitle())
-                .description(book.getDescription())
-                .page(book.getPage())
-                .isbn(book.getIsbn())
-                .publishDate(book.getPublishDate())
-                .publisher(book.getPublisher())
-                .build();
-        return bookResponseDTO;
-    }
+	@Override
+	public BookResponseDTO searchBookDetail(String isbn) {
+		BookEntity book = bookRepository.findByIsbn(isbn)
+			.orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
+		BookResponseDTO bookResponseDTO = BookResponseDTO.builder()
+			.aladinUrl(book.getAladinUrl())
+			.price(book.getPrice())
+			.author(book.getAuthor())
+			.coverImageUrl(book.getCoverImageUrl())
+			.title(book.getTitle())
+			.description(book.getDescription())
+			.page(book.getPage())
+			.isbn(book.getIsbn())
+			.publishDate(book.getPublishDate())
+			.publisher(book.getPublisher())
+			.build();
+		return bookResponseDTO;
+	}
 
-    @Override
-    public List<String> getAutocompleteTitle(String keyword) {
-        List<BookEntity> bookList = bookRepository.findTop5ByTitleContaining(keyword);
-        List<String> titleList = bookList.stream()
-                .map(BookEntity::getTitle)
-                .collect(Collectors.toList());
-        return titleList;
-    }
+	@Override
+	public List<String> getAutocompleteTitle(String keyword) {
+		List<BookEntity> bookList = bookRepository.findTop5ByTitleContaining(keyword);
+		List<String> titleList = bookList.stream()
+			.map(BookEntity::getTitle)
+			.collect(Collectors.toList());
+		return titleList;
+	}
 
-    @Override
-    public List<BookSearchResponseDTO> searchBookList(String keyword) {
-        List<BookEntity> bookList = bookRepository.findByTitleContaining(keyword);
-        List<BookSearchResponseDTO> result = bookList.stream()
-                .map(book -> BookSearchResponseDTO.builder()
-                        .aladinUrl(book.getAladinUrl())
-                        .author(book.getAuthor())
-                        .publishDate(book.getPublishDate())
-                        .title(book.getTitle())
-                        .isbn(book.getIsbn())
-                        .coverImageUrl(book.getCoverImageUrl())
-                        .price(book.getPrice())
-                        .publisher(book.getPublisher())
-                        .build())
-                .collect(Collectors.toList());
-        return result;
-    }
-
+	@Override
+	public Page<BookSearchResponseDTO> searchBookList(String keyword, Pageable pageable) {
+		Page<BookEntity> bookList = bookRepository.findByTitleContaining(keyword, pageable);
+		Page<BookSearchResponseDTO> result = bookList.map(book -> BookSearchResponseDTO.builder()
+			.aladinUrl(book.getAladinUrl())
+			.author(book.getAuthor())
+			.publishDate(book.getPublishDate())
+			.title(book.getTitle())
+			.isbn(book.getIsbn())
+			.coverImageUrl(book.getCoverImageUrl())
+			.price(book.getPrice())
+			.publisher(book.getPublisher())
+			.build());
+		return result;
+	}
 
 }
