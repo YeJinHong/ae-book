95a63e5b24b9d7f05dbbbdc4b60f84b3d3118e47
한선영 <gyeoul98@naver.com>
2023-04-24 14:20:20 +0900

    feat: AuthService 내 Access Token 재발행 기능 추가
    AuthService 내에  Redis를 활용하여 Refresh Token정보를 저장하고, Access Token 재발행 기능 추가하였습니다.
    S08P31C201-400

--- backend/src/main/java/com/c201/aebook/api/auth/service/AuthService.java
+++ backend/src/main/java/com/c201/aebook/api/auth/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.c201.aebook.api.auth.service;
 
 import com.c201.aebook.api.common.LoginUserInfoDTO;
+import com.c201.aebook.api.common.TokenDTO;
+import com.c201.aebook.api.vo.TokenSO;
 import com.c201.aebook.auth.dto.KakaoTokenDTO;
 import com.c201.aebook.auth.profile.KakaoProfile;
+
+import javax.servlet.http.HttpServletRequest;
 
 public interface AuthService {
 
@@ -11,6 +15,10 @@ public interface AuthService {
     public KakaoProfile findProfile(String token);
 
     public LoginUserInfoDTO saveUserOrLogin(String token);
+
+    public String resolveToken(HttpServletRequest request, String header);
+
+    public TokenDTO reissueAccessToken(TokenSO tokenSO);
 
 
 }
