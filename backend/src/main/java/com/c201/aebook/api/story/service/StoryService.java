7ebbace8269005ac5688079989a3a8712f310d28
홍예진 <teepij@naver.com>
2023-04-24 16:11:42 +0900

    feat: 로그인 유저의 동화 리스트 기능 추가
    로그인 유저의 동화 리스트를 반환합니다. 동화 삽화(imgUrl)은 한 개를 반환합니다.
    S08P31C201-335

--- backend/src/main/java/com/c201/aebook/api/story/service/StoryService.java
+++ backend/src/main/java/com/c201/aebook/api/story/service/StoryService.java
@@ -1,7 +1,13 @@
 package com.c201.aebook.api.story.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.c201.aebook.api.story.presentation.dto.response.StoryResponseDTO;
 import com.c201.aebook.api.vo.StorySO;
 
 public interface StoryService {
 	public void saveStory(StorySO storySO);
+
+	Page<StoryResponseDTO> getStoryList(Long userId, Pageable pageable);
 }
--- backend/src/main/java/com/c201/aebook/api/story/service/impl/StoryServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/story/service/impl/StoryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.c201.aebook.api.story.service.impl;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.c201.aebook.api.story.persistence.entity.StoryEntity;
 import com.c201.aebook.api.story.persistence.repository.StoryRepository;
+import com.c201.aebook.api.story.presentation.dto.response.StoryResponseDTO;
 import com.c201.aebook.api.story.service.StoryService;
 import com.c201.aebook.api.user.persistence.entity.UserEntity;
 import com.c201.aebook.api.user.persistence.repository.UserRepository;
@@ -30,6 +33,25 @@ public class StoryServiceImpl implements StoryService {
 			.content(storySO.getContent())
 			.imgUrl(storySO.getImgUrl())
 			.user(user)
+			.build());
+	}
+
+	@Override
+	public Page<StoryResponseDTO> getStoryList(Long userId, Pageable pageable) {
+		// 1. User 유효성 검증
+		UserEntity userEntity = userRepository.findById(userId)
+			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+		// TODO : fetch join이 안되는 관계로 stream 내에서 nickname을 찾는 것으로 임시 구현... 방법 찾기
+		Page<StoryEntity> stories = storyRepository.findAllByUserId(userId, pageable);
+		return stories.map(a -> StoryResponseDTO.builder()
+			.storyId(a.getId())
+			.storyAuthorNickname(userRepository.findById(a.getUser().getId()).get().getNickname())
+			.title(a.getTitle())
+			.content(a.getContent())
+			.createAt(a.getCreatedAt())
+			.updateAt(a.getUpdatedAt())
+			.imgUrl(a.getImgUrl())
 			.build());
 	}
 }
