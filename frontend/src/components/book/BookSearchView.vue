d13da3cda6afc64abf991049618b378035045e91
문희주 <m5nacar5n@gmail.com>
2023-04-25 17:59:50 +0900

    feat: 특정 도서 서평 리스트 axios
    S08P31C201-449
    
    
    Former-commit-id: 9bf2fa19701e2e1e87038c2d4a99376603dc991a

--- frontend/src/components/book/BookSearchView.vue
+++ frontend/src/components/book/BookSearchView.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="container">책 검색</div>
+  <div class="container">책 검색
+    <button @click="getBookReviewList">특정 도서의 서평 리스트</button>
+  </div>
 </template>
 
 <script>
+import http from '@/util/http-common'
 export default {
-  name: 'BookSearchView'
+  name: 'BookSearchView',
+  data () {
+    return {
+      isbn: '9788932916378'
+    }
+  },
+  methods: {
+    getBookReviewList () {
+      http.get(`/reviews/${this.isbn}`).then((result) => {
+        console.log(result)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
