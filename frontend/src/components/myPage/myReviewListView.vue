d70d577d30a90625067de967816e8f61db63073e
문희주 <m5nacar5n@gmail.com>
2023-04-25 18:01:26 +0900

    feat: mypageview 생성
    S08P31C201-447
    
    
    Former-commit-id: bfd7e1e8f5d392424cb3e64182e9851eddecc848

---
+++ frontend/src/components/myPage/myReviewListView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>마이페이지 리뷰 리스트
+    <button @click="getMyReviewList">로그인 사용자의 리뷰 리스트</button>
+  </div>
+</template>
+
+<script>
+import http from '@/util/http-common'
+// import axios from 'axios'
+
+export default {
+  name: 'MyReviewListView',
+  data () {
+    return {
+      page: 0,
+      size: 3,
+      sort: 'id,DESC'
+    }
+  },
+  methods: {
+    getMyReviewList () {
+      // const res = axios({
+      //   method: 'get',
+      //   url: `http://localhost:8082/reviews?page=${this.page}&size=${this.size}&sort=${this.sort}`,
+      //   headers: {
+      //     'Authorization': `Bearer ${this.jwtToken}`,
+      //     'Content-Type': 'application/json;charset=utf-8'
+      //   }
+      // })
+      // console.log(res)
+
+      console.log('getMyReviewList')
+      http.get(`/reviews?page=${this.page}&size=${this.size}&sort=${this.sort}`).then((result) => {
+        console.log(result)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
