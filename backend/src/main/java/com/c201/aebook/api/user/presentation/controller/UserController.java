21ae92d60033628ace5ff2863f70002d1e7330cb
한선영 <gyeoul98@naver.com>
2023-04-25 11:07:11 +0900

    feat: 닉네임 중복 검사 기능 구현
    닉네임 중복 검사 기능을 구현하였습니다. 중복이 아니라면 true를 반환합니다.
    S08P31C201-401
    
    
    Former-commit-id: 06355c976f6ccd0e1b4a436b440add81ffedd0f5

--- backend/src/main/java/com/c201/aebook/api/user/presentation/controller/UserController.java
+++ backend/src/main/java/com/c201/aebook/api/user/presentation/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.c201.aebook.api.user.presentation.controller;
 
+import com.c201.aebook.api.common.BaseResponse;
+import com.c201.aebook.api.user.service.impl.UserServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name="회원관리")
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @Operation(summary = "닉네임 중복 검사", description = "사용자의 닉네임 중복 검사를 합니다.")
+    @PostMapping(path = "/exists")
+    public BaseResponse<?> checkUserNickname(
+            @RequestParam(name = "nickname") String nickname
+    ) {
+        userService.duplicatedUserByNickname(nickname); // 닉네임 중복 검사\
+        return new BaseResponse<>(true, HttpStatus.OK.value(), "닉네임 중복 없음");
+    }
+
 }
