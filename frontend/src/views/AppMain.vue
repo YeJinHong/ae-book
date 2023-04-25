b1dca7c7df6f5861a8c8acf27f6db003ce71b962
문희주 <m5nacar5n@gmail.com>
2023-04-25 17:59:25 +0900

    feat: 메인페이지 최신 서평 리스트 axios
    S08P31C201-449
    
    
    Former-commit-id: baa1f75eca06110a48c0fb477a9338294eceac3e

--- frontend/src/views/AppMain.vue
+++ frontend/src/views/AppMain.vue
@@ -5,17 +5,28 @@
     <!-- TODO : 각기 다음에 필요한 컴포넌트는 components/{domain} 하위에 작성 -->
     <!-- TODO : 새로 들어온 책 콘텐츠 케러셀 컴포넌트-->
     <!-- TODO : 최근 리뷰 컨텐츠 케러셀 컴포넌트 -->
+    <button @click="getLatestReviewList">최신 리뷰 리스트</button>
     <!-- TODO : 우리 아이들 작품(만든 동화) 리스트 컴포넌트 -->
   </div>
 </template>
 
 <script>
+import http from '@/util/http-common'
 import HeaderNavbarMain from '@/components/HeaderNavbarMain'
 
 export default {
   name: 'AppMain',
   components: {
     HeaderNavbarMain
+  },
+  methods: {
+    getLatestReviewList () {
+      http.get(`/reviews/latest`).then((result) => {
+        console.log(result)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
   }
 }
 
