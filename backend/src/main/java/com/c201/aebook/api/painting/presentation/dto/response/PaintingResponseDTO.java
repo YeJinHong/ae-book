7ff58cbd5161dd2ec3175d50c8fdb1ef15923605
leedoyeon <ldy__0202@naver.com>
2023-04-25 15:37:55 +0900

    feat: 그림 리스트 조회
    S08P31C201-357
    
    
    Former-commit-id: aef561637aab4a4cf26fea6e688ff8f9ce1d1f18

--- backend/src/main/java/com/c201/aebook/api/painting/presentation/dto/response/PaintingResponseDTO.java
+++ backend/src/main/java/com/c201/aebook/api/painting/presentation/dto/response/PaintingResponseDTO.java
@@ -1,5 +1,10 @@
 package com.c201.aebook.api.painting.presentation.dto.response;
 
+import java.time.LocalDateTime;
+
+import com.c201.aebook.api.painting.persistence.entity.PaintingType;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,5 +13,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class PaintingResponseDTO {
+	@Schema(description = "그림 제목", defaultValue = "내가 그린 공룡 그림")
+	private String title;
 
+	@Schema(description = "그림 이미지 URL", defaultValue = "https://aebookbucket.s3.ap-northeast-2.amazonaws.com/6/paintings/Image%20Pasted%20at%202023-4-24%2010-22.png")
+	private String fileUrl;
+	@Schema(description = "그림 타입", defaultValue = "COLOR")
+	private PaintingType type;
+	@Schema(description = "그림 저장 시각", defaultValue = "2023-04-14 10:30:15")
+	private LocalDateTime createAt;
+
+	@Schema(description = "그림 제목 수정 시각", defaultValue = "2023-04-17 10:30:15")
+	private LocalDateTime updateAt;
 }
