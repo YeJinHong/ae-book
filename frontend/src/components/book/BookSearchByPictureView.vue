e9c9d218d4ab8d61434b24b93fa487bc65d95b20
문희주 <m5nacar5n@gmail.com>
2023-04-25 17:58:42 +0900

    feat: 특정 도서 서평 리스트 axios 연결
    S08P31C201-448
    
    
    Former-commit-id: ddcb516e802e46e83aab51fe257a4771df37bb82

--- frontend/src/components/book/BookSearchByPictureView.vue
+++ frontend/src/components/book/BookSearchByPictureView.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>사진으로 책 검색</div>
+  <div>사진으로 책 검색
+    <!-- <button @click="">사진으로 책 검색</button> -->
+  </div>
 </template>
 
 <script>
+import http from '@/util/http-common'
+
 export default {
-  name: 'BookSearchByPictureView'
+  name: 'BookSearchByPictureView',
+  data () {
+    return {
+      isbn: '9788932916378'
+    }
+  },
+  methods: {
+    // 이거 다른 곳에서도 계속 쓰는거라 .... 흠 어떻게 해야될지 .... ㅠㅅㅠ
+    getBookReviewList () {
+      http.get(`/reviews/${this.isbn}`).then((result) => {
+        console.log(result)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
