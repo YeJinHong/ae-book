049f2230add77e595c36ee0bd765a75e7677211c
한선영 <gyeoul98@naver.com>
2023-04-25 15:24:11 +0900

    feat: getProfileImage과 updateUserInfo 기능 구현
    사용자 아이디로 프로필 이미지 찾는 getProfileImage과 사용자 정보를 변경하는 updateUserInfo 기능 구현
    S08P31C201-401
    
    
    Former-commit-id: 917218960f5c985883a107842198327658aad3d6

--- backend/src/main/java/com/c201/aebook/api/user/service/UserService.java
+++ backend/src/main/java/com/c201/aebook/api/user/service/UserService.java
@@ -1,7 +1,14 @@
 package com.c201.aebook.api.user.service;
 
+import com.c201.aebook.api.user.presentation.dto.response.UserResponeDTO;
+import com.c201.aebook.api.vo.UserSO;
+
 public interface UserService {
 
     public void duplicatedUserByNickname(String nickname);
+
+    public String getProfileImage(long userId);
+
+    public UserResponeDTO updateUserInfo(long userId, UserSO userSO);
 
 }
