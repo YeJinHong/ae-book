f184b4064114850a8b152fd4222eec911609f474
문희주 <m5nacar5n@gmail.com>
2023-04-25 10:55:56 +0900

    feat: SearchBook component
    book으로 묶은 친구들을 컴포넌트로 생성
    S08P31C201-322
    
    
    Former-commit-id: 9376f1e734eb0462334a5b50368ab23fd7bbd6e5

---
+++ frontend/src/components/book/BookSearchView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">책 검색</div>
+</template>
+
+<script>
+export default {
+  name: 'BookSearchView'
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 0 100px;
+}
+</style>
