cfb0942b7ada89792b0322b11d638bd7f0121320
홍예진 <teepij@naver.com>
2023-04-25 12:41:05 +0900

    feat: 동화 제목 변경 기능 추가 동화 제목 변경에 builder 패턴 사용 Patch DTO, SO 생성
    S08P31C201-355

--- backend/src/main/java/com/c201/aebook/converter/StoryConverter.java
+++ backend/src/main/java/com/c201/aebook/converter/StoryConverter.java
@@ -1,13 +1,13 @@
 package com.c201.aebook.converter;
 
-import java.time.LocalDateTime;
-
 import org.mapstruct.Mapper;
 
 import com.c201.aebook.api.story.persistence.entity.StoryEntity;
+import com.c201.aebook.api.story.presentation.dto.request.StoryPatchRequestDTO;
 import com.c201.aebook.api.story.presentation.dto.request.StoryRequestDTO;
 import com.c201.aebook.api.story.presentation.dto.response.StoryResponseDTO;
 import com.c201.aebook.api.vo.StoryDeleteSO;
+import com.c201.aebook.api.vo.StoryPatchSO;
 import com.c201.aebook.api.vo.StorySO;
 
 @Mapper(componentModel = "spring")
@@ -16,9 +16,8 @@ public interface StoryConverter {
 
 	StoryDeleteSO toStoryDeleteSO(Long userId, Long storyId);
 
-	StoryResponseDTO toStoryResponseDTO(StoryEntity storyEntity, Long storyId, String storyAuthorNickname,
-		LocalDateTime createdAt, LocalDateTime updatedAt);
-
 	StoryResponseDTO toStoryResponseDTO(StoryEntity storyEntity, Long storyId, String storyAuthorNickname);
+
+	StoryPatchSO toStoryPatchSO(String userId, Long storyId, StoryPatchRequestDTO storyPatchRequestDTO);
 
 }
