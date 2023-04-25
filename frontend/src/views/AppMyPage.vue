552fdb308c961488b6c85da56563fbc4586f0036
문희주 <m5nacar5n@gmail.com>
2023-04-25 18:01:26 +0900

    feat: mypageview 생성
    S08P31C201-447
    
    
    Former-commit-id: bfd7e1e8f5d392424cb3e64182e9851eddecc848

---
+++ frontend/src/views/AppMyPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header-navbar></header-navbar>
+    <div>This is MyPage</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HeaderNavbar from '../components/HeaderNavbar.vue'
+
+export default {
+  name: 'AppMyPage',
+  components: {
+    HeaderNavbar
+  }
+}
+</script>
+
+<style>
+
+</style>
