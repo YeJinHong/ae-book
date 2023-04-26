fcf32b9e5407b6d4cbc25aa9f812a00d2e8072f7
한선영 <gyeoul98@naver.com>
2023-04-26 12:10:25 +0900

    feat: footer 생성
    사업자 정보를 포함한 footer 생성하였습니다.
    S08P31C201-405
    
    
    Former-commit-id: b1266ce48589329026f7bfbf19c474b4b05b26a1

---
+++ frontend/src/components/FooterInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <br>&copy;아이북(aebook) C201
+    <br>상호명 : 아이북
+    <br>사업자 등록 번호 : 746-43-01140
+    <br>사업자명 : 이도연
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterInfo'
+}
+</script>
+
+<style scoped>
+
+</style>
