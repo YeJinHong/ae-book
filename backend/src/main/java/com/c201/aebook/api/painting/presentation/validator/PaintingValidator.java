276051d1481fb02b546d191eaab48da83759bd36
leedoyeon <ldy__0202@naver.com>
2023-04-26 13:26:21 +0900

    feat: 그림 제목 수정 기능 추가
    S08P31C201-330
    
    
    Former-commit-id: 9e34b517ac8f40072d042c79553dc9661a9ff14a

--- backend/src/main/java/com/c201/aebook/api/painting/presentation/validator/PaintingValidator.java
+++ backend/src/main/java/com/c201/aebook/api/painting/presentation/validator/PaintingValidator.java
@@ -4,11 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.c201.aebook.api.painting.presentation.dto.request.PaintingRequestDTO;
+import com.c201.aebook.api.painting.presentation.dto.request.PaintingTitleRequestDTO;
 
 @Component
 public class PaintingValidator {
 	public void validatePaintingRequestDTO(PaintingRequestDTO paintingRequestDTO) {
 		checkStringType(paintingRequestDTO.getTitle(), "그림 제목");
+	}
+
+	public void validatePaintingTitleRequestDTO(PaintingTitleRequestDTO paintingTitleRequestDTO) {
+		checkStringType(paintingTitleRequestDTO.getTitle(), "그림 제목");
 	}
 
 	public void checkStringType(String value, String name) {
