b275e87c4017764e2a461e7560ce2417dc9b6541
이해민 <2haemin98@gmail.com>
2023-04-27 13:42:44 +0900

    feat : 동화 키워드 입력 화면 구현
    동화 키워드 입력 화면 구현
    S08P31C201-403
    
    
    Former-commit-id: a5cf9a9e5738a5e205b1ade8703054f4f931a71a

---
+++ frontend/src/components/story/StoryKeywordInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" v-model="inputValue">
+    <button @click="sendInputValue">전송</button>
+    <StoryChatGptView :storyResult="storyResult"></StoryChatGptView>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import StoryChatGptView from './StoryChatGptView.vue'
+
+export default {
+  components: {
+    StoryChatGptView
+  },
+  data () {
+    return {
+      inputValue: '',
+      storyResult: String
+    }
+  },
+  methods: {
+    sendInputValue () {
+      axios.post(`/stories/gpt`, {value: this.inputValue}).then((result) => {
+        console.log(result)
+        this.storyResult = result
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
