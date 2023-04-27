7eee4a064059c929ff905cda243ff9331a3b330b
이해민 <2haemin98@gmail.com>
2023-04-27 15:10:00 +0900

    feat : 그림 조회 component
    그림 조회 component
    S08P31C201-403
    
    
    Former-commit-id: cd6319cd985c71e204a71a234f6fcb2a51d01b6e

---
+++ frontend/src/components/story/StoryList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+export default {
+  name: 'StroyList',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    showList () {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
