9a5d8691f530bbf70cefea607e5e95bc719eedbe
한선영 <gyeoul98@naver.com>
2023-04-27 15:24:13 +0900

    feat: 로그아웃 기능 추가
    로그아웃 기능을 하는 버튼을 추가하였습니다.
    S08P31C201-405
    
    
    Former-commit-id: 97d94aef725f19ec3360326f31106b4a468e8f1a

--- frontend/src/components/HeaderNavbarMain.vue
+++ frontend/src/components/HeaderNavbarMain.vue
@@ -32,11 +32,17 @@
                     </router-link>
                 </b-nav-item>
                 </b-navbar-nav>
-                <!-- TODO : 로그인 여부(세션에 JWT 정보 확인)에 따라(v-if) 로그인/마이페이지로 출력 바꾸기-->
+                <!-- 로그인 후 -->
                 <b-navbar-nav class="ml-auto">
                     <b-nav-form>
                         <!-- TODO : 로그인 모달창 작성 및 연결 -->
-                        <b-button size="sm" @click="login">로그인</b-button>
+                        <b-button size="sm" @click="loginOut">로그아웃</b-button>
+                    </b-nav-form>
+                </b-navbar-nav>
+                <!-- 로그인 전 -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-button size="sm"><router-link :to="{ name: 'Login' }" class="link">로그인 </router-link></b-button>
                     </b-nav-form>
                 </b-navbar-nav>
             </b-collapse>
@@ -48,10 +54,24 @@
 // TODO: 메인페이지 Navbar 배치 및 디자인 변경
 export default {
   name: 'HeaderNavbarMain',
+  data () {
+    return {
+      kakaoLogOutLink: `https://kauth.kakao.com/oauth/logout?client_id=${process.env.VUE_APP_REST_API_KEY}&logout_redirect_uri=${process.env.VUE_APP_LOGOUT_REDIRECT_URI}`,
+      islogin: false
+    }
+  },
 
   methods: {
-    login () {
-      this.$router.push({ name: 'Login' })
+    // login () {
+    //   this.$router.push({ name: 'Login' })
+    // },
+    loginOut () {
+      console.log('로그아웃')
+      if (window.confirm('로그아웃 하시겠습니까?')) {
+        localStorage.removeItem('authorization')
+        localStorage.removeItem('refresh')
+        window.location.href = this.kakaoLogOutLink
+      }
     }
   }
 }
