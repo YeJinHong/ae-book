c096bb2e4b6ef32ca3ebbee4df82c4f4e20f4d31
문희주 <m5nacar5n@gmail.com>
2023-04-27 17:49:14 +0900

    feat: 특정 서평 조회 기능 구현
    현재 1번 서평으로 하드코딩 됨
    리스트 뿌리면 바꿀 예정
    S08P31C201-456
    
    
    Former-commit-id: 88808293f38984acff42747de14f50c43e9ca900

--- frontend/src/components/review/ReviewDetailView.vue
+++ frontend/src/components/review/ReviewDetailView.vue
@@ -1,29 +1,53 @@
 <template>
   <div>
     <div>ReviewDetail components</div>
-    <!-- 리뷰 아이디 리스트 -->
-
     <!-- 리뷰 디테일 -->
+    <div v-if="review">
+     reviewId : {{ review.reviewId }}  <br/>
+     작성자 : {{ review.reviewerNickname }} <br/>
+     별점 : {{ review.score }} <br/>
+     내용 : {{ review.content }} <br/>
+     작성일 : {{ review.createAt }} <br />
+     수정일 : {{ review.updateAt }}
+    </div>
     <!-- "result": {
-        "reviewId": 4,
-        "reviewerId": 1,
-        "score": 3,
-        "content": "서평 내용",
-        "createAt": "2023-04-20T10:33:46.069609",
-        "updateAt": "2023-04-20T10:33:46.069609"
+        "reviewId": 23,
+        "reviewerNickname": "닉네임2",
+        "score": 5,
+        "content": "억뷰",
+        "createAt": "2023-04-21T08:15:55",
+        "updateAt": "2023-04-21T08:15:55"
+    },
     }, -->
   </div>
 </template>
 
 <script>
+import { mapState, mapActions, mapGetters } from 'vuex'
+const reviewStore = 'reviewStore'
+
 export default {
   name: 'ReviewDetailView',
+  props: ['reviewId'],
   data () {
     return {
-
+      // review: Object
     }
+  },
+  methods: {
+    ...mapActions(reviewStore, ['getReviewAction'])
+  },
+  computed: {
+    ...mapGetters(reviewStore, ['getReview']),
+    ...mapState(reviewStore, ['review'])
+  },
+  mounted () {
+    this.getReviewAction(this.reviewId)
+    // this.review = this.getReview
+  },
+  created () {
+    console.log('created: ' + this.$route.params.reviewId)
   }
-
 }
 </script>
 
