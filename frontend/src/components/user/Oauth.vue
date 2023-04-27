0390f1730dc670f9f80bc578b8b143bc88706b14
한선영 <gyeoul98@naver.com>
2023-04-27 10:37:31 +0900

    feat: 로그인 기능 구현
    url에 있는 카카오 인가 코드를 받아서 axios로 로그인을 요청하는 기능을 구현하였습니다. token 정보는 localStorage에 저장 되도록 하였습니다.
    S08P31C201-405
    
    
    Former-commit-id: d4765de403ce6931831f36dad42ef6fe7d830734

---
+++ frontend/src/components/user/Oauth.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div>oauth</div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'oauth',
+  data () {
+    return {
+      // isLoginError: false,
+      kakaoCode: ''
+    }
+  },
+
+  created () {
+    // console.log(this.$route.query) // 경로 확인
+    this.kakaoCode = this.$route.query
+    // console.log(this.kakaoCode.code)
+    // console.log(typeof this.kakaoCode.code)
+
+    if (this.kakaoCode.code === undefined) {
+      console.log('undifined')
+    } else {
+      console.log('axios')
+      axios.get(`/api/auth/login?code=${this.kakaoCode.code}`).then((result) => {
+        console.log(result)
+        // console.log(result.headers.authorization)
+        // console.log(result.headers.refresh)
+        localStorage.setItem('authorization', result.headers.authorization)
+        localStorage.setItem('refresh', result.headers.refresh)
+        this.$router.push({ name: 'Main' })
+      })
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
