59eb09f8dad82c68ce289bfb8d81661811bf63d3
문희주 <m5nacar5n@gmail.com>
2023-04-27 17:48:20 +0900

    feat: 서평 모듈 등록
    S08P31C201-456
    
    
    Former-commit-id: 13b896d6ffa625e0016e8c81bcfd8b1cc60488bc

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import bookStore from '@/store/modules/bookStore.js'
+import reviewStore from './modules/reviewStore'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    bookStore
+    bookStore,
+    reviewStore
   }
 })
 
