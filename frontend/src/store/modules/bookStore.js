5579fb2c5877cc266685e220b0a851b346800727
leedoyeon <ldy__0202@naver.com>
2023-04-27 15:19:29 +0900

    feat: vuex 적용
    S08P31C201-463
    
    
    Former-commit-id: 46ebbe02043f2e8f10f394e5267a83f5509ed92e

---
+++ frontend/src/store/modules/bookStore.js
@@ -0,0 +1,52 @@
+import { searchByISBN } from '@/api/book';
+
+const bookStore = {
+  namespaced: true,
+  // state: 변수들의 집합
+  state: {
+    book: null,
+    bookList: []
+  },
+  /*
+  Gettes: state의 변수들을 get하는역할을 한다.
+  - state 자체를 변경하지 않음.
+  */
+  getters: {
+    getBook: state => {
+      return state.book;
+    },
+    getBookList: state => {
+      return state.bookList;
+    }
+  },
+  /*
+  Mutations: 변수들을 조작하는 함수들의 집합
+  - State는 반드시 Mutations가 가진 method를 통해서만 조작 함.
+  */
+  mutations: {
+    SET_BOOK: (state, data) => {
+      state.book = data;
+    },
+    SET_BOOK_LIST: (state, data) => {
+      state.bookList = data;
+    }
+  },
+  /*
+  Actions: 비동기 처리를 하는 함수들의 집합
+  - Actions에서는 비동기적 작업을 Mutations에서는 동기적 작업만을 함.
+  */
+  actions: {
+    getBookDetail ({ commit }, isbn) {
+      searchByISBN(isbn)
+        .then(({ data }) => {
+          commit('SET_BOOK', data.result);
+          console.log(data.result);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+
+export default bookStore;
