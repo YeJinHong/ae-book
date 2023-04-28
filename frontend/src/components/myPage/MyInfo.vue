1f2b060d2341002728297b9f2e69ca2e7b90bd88
한선영 <gyeoul98@naver.com>
2023-04-28 13:37:33 +0900

    feat: 마이페이지 컴포넌트 생성
    마이페이지 컴포넌트를 생성하였습니다.
    S08P31C201-407
    
    
    Former-commit-id: 3e294d2333192261ec474c5b79445a0a6e4d666e

---
+++ frontend/src/components/myPage/MyInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>사용자 정보</div>
+    <div id="img"><img :src=user.profileUrl></div>
+    <div>{{ user.nickname }}</div>
+    <!-- <b-button size="sm" @click="modifyUserInfo">사용자 정보 수정</b-button> -->
+    <!-- 임시로 page를 생성하고 모달로 변경하도록 하겠습니다,,,-->
+    <router-link :to="{ name: 'MyInfoModify' }" class="link"><b-button size="sm">사용자 정보 수정</b-button></router-link>&nbsp;
+    <b-button size="sm" @click="deleteUserInfo">사용자 탈퇴</b-button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+const userStore = 'userStore'
+
+export default {
+  name: 'MyInfo',
+  created () {
+    // console.log(this.isLogin)
+    this.isLoginUser = sessionStorage.getItem('isLoginUser')
+  },
+  computed: {
+    ...mapState(userStore, ['isLogin', 'isLoginError', 'user']),
+    user () {
+      return JSON.parse(sessionStorage.getItem('userInfo'))
+    }
+  },
+
+  methods: {
+    modifyUserInfo () {
+      console.log('사용자 정보 수정')
+    },
+    deleteUserInfo () {
+      console.log('사용자 탈퇴')
+    }
+  }
+
+}
+</script>
+
+<style>
+#img img {
+  width: 200px;
+  height: 200px;
+}
+</style>
