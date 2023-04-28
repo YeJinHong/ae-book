8775a73644e145ab17dde47e661b42db6e03e529
한선영 <gyeoul98@naver.com>
2023-04-28 09:28:57 +0900

    feat: 로그인 로그아웃 api 추가
    로그인, 로그아웃 기능을 위해 요청하는 axios 코드를 작성하였습니다.
    S08P31C201-405
    
    
    Former-commit-id: a065d41e3e84a332962712b6c38a6d6899c595ab

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,5 +1,24 @@
 import axios from 'axios'
+import api from '@/api/auth'
 
-const login = kakaoCode => axios.get(`/api/auth/login?code=${this.kakaoCode.code}`)
+const login = async (kakaoCode) => {
+  try {
+    const response = await axios.get(`/api/auth/login?code=${kakaoCode}`)
+    return response
+  } catch (error) {
+    console.log(error)
+    throw new Error('로그인에 실패하였습니다.')
+  }
+}
+const logout = async () => {
+  try {
+    console.log('로그아웃')
+    const response = api.post(`/api/auth/logout`)
+    return response
+  } catch (error) {
+    console.log(error)
+    throw new Error('로그아웃에 실패하였습니다.')
+  }
+}
 
-export { login }
+export { login, logout }
