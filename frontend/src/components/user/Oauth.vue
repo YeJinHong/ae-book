234af6c9d009f2db7c53358ad3dca5ba62f09ff4
한선영 <gyeoul98@naver.com>
2023-04-28 09:50:01 +0900

    feat: 로그인 기능에서 userStore를 사용하도록 변경
    로그인 기능에서 바로 axios를 사용하지 않고, userStore를 사용하도록 변경하였습니다.
    S08P31C201-405
    
    
    Former-commit-id: 232c16d76f3d1b9f5182384b1f1df2893185f3dc

--- frontend/src/components/user/Oauth.vue
+++ frontend/src/components/user/Oauth.vue
@@ -5,10 +5,8 @@
 </template>
 
 <script>
-import axios from 'axios'
-
-// import { mapState, mapActions } from 'vuex'
-// const userStore = 'userStore'
+import { mapState, mapActions } from 'vuex'
+const userStore = 'userStore'
 
 export default {
   name: 'oauth',
@@ -22,33 +20,24 @@ export default {
   created () {
     // console.log(this.$route.query) // 경로 확인
     this.kakaoCode = this.$route.query
-    // console.log(this.kakaoCode.code)
-    // console.log(typeof this.kakaoCode.code)
 
     if (this.kakaoCode.code === undefined) {
       console.log('undifined')
     } else {
-      console.log('axios')
-      axios.get(`/api/auth/login?code=${this.kakaoCode.code}`).then((result) => {
-        console.log(result)
-        // console.log(result.headers.authorization)
-        // console.log(result.headers.refresh)
-        localStorage.setItem('authorization', result.headers.authorization)
-        localStorage.setItem('refresh', result.headers.refresh)
-        this.$router.push({ name: 'Main' })
-      })
+      // console.log(this.kakaoCode.code)
+      this.login(this.kakaoCode.code)
+    }
+  },
+  computed: {
+    ...mapState(userStore, ['isLogin', 'isLoginError', 'user'])
+  },
+  methods: {
+    ...mapActions(userStore, ['userLogin']),
+    async login () {
+      await this.userLogin(this.kakaoCode.code)
+      this.$router.push({ name: 'Main' })
     }
   }
-  // computed: {
-  //   ...mapState(userStore, ['isLogin', 'isLoginError', 'user'])
-  // },
-  // methods: {
-  //   ...mapActions(userStore, ['userConfirm']),
-  //   async confirm () {
-  //     await this.userConfirm(this.kakaoCode.code)
-  //     this.$router.push({ name: 'Main' })
-  //   }
-  // }
 
 }
 </script>
