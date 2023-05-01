cf73968a2119ad11ab0b9b6a614bce4ae77ea2f2
m5nacar5n <m5nacar5n@gmail.com>
2023-05-02 01:16:59 +0900

    feat: 도서 서평 리스트 컴포넌트 구현
    도연님의 페이지내이션 컴포넌트를 이용하여 구현완료 ^.^
    S08P31C201-469
    
    
    Former-commit-id: 370e93ca24087c2beabe300ff0ce62aa7042990e

--- frontend/src/components/review/ReviewBookListView.vue
+++ frontend/src/components/review/ReviewBookListView.vue
@@ -1,31 +1,72 @@
 <template>
-      <button @click="getBookReviewList">특정 도서의 서평 리스트</button>
+  <div>
+    <div
+      class="list-group-item"
+      v-for="review in reviewBookList"
+      :key="review.id"
+    >
+      <div>
+        {{ review.reviewerNickname }} |
+        {{ review.content }} |
+        {{ review.score }}
+      </div>
+    </div>
+    <div class="pagination-container">
+      <pagination :pageSetting="reviewBookPageSetting" @paging="paging"></pagination>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import { mapActions, mapState, mapGetters } from 'vuex'
+import Pagination from '../common/Pagination.vue'
+
+const reviewStore = 'reviewStore'
 
 export default {
   name: 'ReviewBookListView',
+  components: {
+    Pagination
+  },
   props: ['isbn'],
   data () {
     return {
-
+      request: {
+        isbn: this.isbn,
+        page: 0,
+        size: 3,
+        sort: 'id',
+        direction: 'DESC'
+      }
     }
   },
   methods: {
-    getBookReviewList () {
-      console.log(this.isbn)
-      axios.get(`/api/reviews/${this.isbn}`).then((result) => {
-        console.log(result)
-      }).catch((err) => {
-        console.log(err)
-      })
+    ...mapActions(reviewStore, ['getReviewBookListAction', 'getReviewBookPageAction']),
+    paging (page) {
+      this.request['page'] = page - 1
+      this.getReviewBookListAction(this.request)
     }
+  },
+  computed: {
+    ...mapState(reviewStore, ['reviewBookList', 'reviewBookPageSetting']),
+    ...mapGetters(reviewStore, ['getReviewBookList'])
+  },
+  mounted () {
+    const request = {
+      isbn: this.isbn,
+      page: 0,
+      size: 3,
+      sort: 'id',
+      direction: 'DESC'
+    }
+    this.getReviewBookListAction(request)
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.pagination-container {
+  display:flex;
+  justify-content: center;
+}
 </style>
