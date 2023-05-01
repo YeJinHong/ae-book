c3a2da80e56dc648d5373b48f1c03c50ec4d2ffe
이해민 <2haemin98@gmail.com>
2023-05-01 13:24:26 +0900

    feat : modal 컴포넌트 구현
    modalview.vue 구현
    S08P31C201-471
    
    
    Former-commit-id: dd5a6844b4de3ac7a9f7a4f160f102e64675efac

---
+++ frontend/src/components/common/ModalView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal-overlay">
+    <b-modal v-model="modalShow" @hidden="onHidden"> </b-modal>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ModalView',
+  props: {
+    modalShow: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    onHidden () {
+      this.$emit('close-modal')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
