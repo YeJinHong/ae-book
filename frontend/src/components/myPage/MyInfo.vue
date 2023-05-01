877b68afcd6a48badf7d2fa291d7dde719ea1231
한선영 <gyeoul98@naver.com>
2023-05-01 16:46:47 +0900

    feat: 사용사 탈퇴 methods 기능 구현
    사용사 탈퇴 methods 기능을 구현하였습니다.
    S08P31C201-409
    
    
    Former-commit-id: d8236171825b4a9b7e2a4a62fc27173c28385554

--- frontend/src/components/myPage/MyInfo.vue
+++ frontend/src/components/myPage/MyInfo.vue
@@ -4,14 +4,14 @@
     <div id="img"><img :src=user.profileUrl></div>
     <div>{{ user.nickname }}</div>
     <!-- <b-button size="sm" @click="modifyUserInfo">사용자 정보 수정</b-button> -->
-    <!-- 임시로 page를 생성하고 모달로 변경하도록 하겠습니다,,,-->
+    <!-- TODO: 임시로 page를 생성하고 모달로 변경하도록 하겠습니다,,,-->
     <router-link :to="{ name: 'MyInfoModify' }" class="link"><b-button size="sm">사용자 정보 수정</b-button></router-link>&nbsp;
     <b-button size="sm" @click="deleteUserInfo">사용자 탈퇴</b-button>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex'
+import { mapGetters, mapState, mapActions } from 'vuex'
 const userStore = 'userStore'
 
 export default {
@@ -30,11 +30,23 @@ export default {
   },
 
   methods: {
-    modifyUserInfo () {
-      console.log('사용자 정보 수정')
-    },
+    ...mapActions(userStore, ['userDelete', 'userLogout']),
     deleteUserInfo () {
       console.log('사용자 탈퇴')
+      if (confirm('정말로 탈퇴하시겠습니까?')) {
+        this.userDelete()
+          .then(() => {
+            console.log('사용자 탈퇴 완료')
+            return this.userLogout()
+          })
+          .then(() => {
+            console.log('사용자 로그아웃 완료')
+            this.$router.push({ name: 'Main' })
+          })
+          .catch((error) => {
+            console.error(error)
+          })
+      }
     }
   }
 
