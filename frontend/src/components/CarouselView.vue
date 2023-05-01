a941ae5609b1137b78de040de35bdf1fcb90b753
문희주 <m5nacar5n@gmail.com>
2023-05-01 15:08:14 +0900

    fix: 크기 수정 및 불필요 내용 삭제
    1. 캐러셀 높이 축소
    2. Review -> Item으로 이름 통일
    3. 필요없는 selectReview 삭제.. 해당 기능을 발생시키는 버튼 없음
    4. 책 상세 페이지로 가는 router 임시 설정
    S08P31C201-467
    
    
    Former-commit-id: 4de1d5ae8bc03355c1bd6a1d366097ea7259daa2

--- frontend/src/components/CarouselView.vue
+++ frontend/src/components/CarouselView.vue
@@ -15,22 +15,26 @@ Copy code
       indicators
       background="#FFE5AB"
       img-width="1024"
-      img-height="480"
+      img-height="300"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
     <!-- 여기에 보여줄 리스트 넣으면 됨 -->
-    <!-- 캐러셀 한 페이지에 N개의 요소를 보여주려고 chunk,,, chunkedReviews()에서 chunkSize 변경 가능 -->
-      <template v-for="(reviewsChunk, index) in chunkedReviews">
+    <!-- 캐러셀 한 페이지에 N개의 요소를 보여주려고 chunk,,, chunkeItems()에서 chunkSize 변경 가능 -->
+      <template v-for="(itemsChunk, index) in chunkedItems">
         <!-- 캐러셀 배경 필요 ... 현재는 img-blank -->
         <b-carousel-slide :key="index" img-blank img-alt="Blank image">
           <div class="d-flex">
-            <div v-for="review in reviewsChunk" :key="review.reviewId" class="chunk w-50">
+            <!-- key를 요소에 맞춰 설정 -->
+            <div v-for="item in itemsChunk" :key="item.id" class="chunk w-50">
               <!-- 리뷰를 나타내는 내용 추가 -->
-              <h3>{{ review.reviewerNickname }}</h3>
-              <h2>{{ review.content }}</h2>
-              <h2>{{ review.score }}</h2>
+              <h3>{{ item.reviewerNickname }}</h3>
+              <h2>{{ item.content }}</h2>
+              <h2>{{ item.score }}</h2>
+              <router-link :to="{ name: 'BookDetail', params: { isbn: item.isbn } }">
+                책 보러가기
+              </router-link>
             </div>
           </div>
         </b-carousel-slide>
@@ -43,7 +47,7 @@ Copy code
 export default {
   name: 'Carousel',
   props: {
-    reviews: {
+    items: {
       type: Array,
       required: true
     }
@@ -55,9 +59,6 @@ export default {
     }
   },
   methods: {
-    selectReview (review) {
-      this.$emit('input', review)
-    },
     onSlideStart (slide) {
       this.sliding = true
     },
@@ -72,10 +73,10 @@ export default {
     }
   },
   computed: {
-    chunkedReviews () {
+    chunkedItems () {
       // 리뷰 리스트를 3개씩 자르기
       const chunkSize = 3
-      return this.reviews.reduce((resultArray, item, index) => {
+      return this.items.reduce((resultArray, item, index) => {
         const chunkIndex = Math.floor(index / chunkSize)
         if (!resultArray[chunkIndex]) {
           resultArray[chunkIndex] = [] // start a new chunk
