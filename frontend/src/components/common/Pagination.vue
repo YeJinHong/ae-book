d34703d3b0f9b02efc454c5ae40a57c7d3497484
leedoyeon <ldy__0202@naver.com>
2023-05-01 10:45:24 +0900

    feat: 페이지네이션 기능 구현
    S08P31C201-468
    
    
    Former-commit-id: 3c69fcf900408b507caa05476729b4ba75d5bfcf

---
+++ frontend/src/components/common/Pagination.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+    <div class="page-list">
+      <button
+        class="pagebtn pre"
+        v-if="!pageSetting.first"
+        @click="sendPage(pageSetting.pageNumber-1)"
+      >이전</button>
+      <ul>
+        <li
+          class="pagebtn"
+          :class="{ active: page === pageSetting.pageNumber }"
+          v-for="page in pageSetting.totalPages"
+          :key="page"
+          @click="sendPage(page)"
+        >
+          {{ page }}
+        </li>
+      </ul>
+      <button
+        class="pagebtn next"
+        v-if="!pageSetting.last"
+        @click="sendPage(pageSetting.pageNumber+1)"
+      >다음</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: ['pageSetting'],
+  mounted () {
+    console.log(this.pageSetting)
+  },
+  methods: {
+    sendPage (page) {
+      this.$emit('paging', page)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
