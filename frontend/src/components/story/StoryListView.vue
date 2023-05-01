2b2d60e869173a6823bffb79c4f316c03b36b4b4
이해민 <2haemin98@gmail.com>
2023-05-01 13:24:26 +0900

    feat : modal 컴포넌트 구현
    modalview.vue 구현
    S08P31C201-471
    
    
    Former-commit-id: dd5a6844b4de3ac7a9f7a4f160f102e64675efac

--- frontend/src/components/story/StoryListView.vue
+++ frontend/src/components/story/StoryListView.vue
@@ -2,20 +2,25 @@
   <div>
     <h2>나의 동화 목록</h2>
     <div v-for="story in storyList" :key="story.storyId">
-      <h3>{{ story.title }}</h3>
+      <h3 @click="showModal(story.storyId)">{{ story.title }}</h3>
       <p>{{ story.content }}</p>
       <img v-bind:src="story.imgUrl" alt="story image" />
       <p>작성자: {{ story.storyAuthorNickname }}</p>
       <p>작성일: {{ story.createdAt }}</p>
     </div>
+    <ModalView :modalShow="isModalVisible" @close-modal="closeModal"></ModalView>
   </div>
 </template>
 
 <script>
 import { searchStory } from '@/api/story'
+import ModalView from '@/components/common/ModalView'
 
 export default {
   name: 'StoryListView',
+  components: {
+    ModalView
+  },
   data () {
     return {
       page: {
@@ -23,7 +28,8 @@ export default {
         size: 8,
         sort: 'id'
       },
-      storyList: []
+      storyList: [],
+      isModalVisible: false
     }
   },
   mounted () {
@@ -34,6 +40,15 @@ export default {
       .catch(error => {
         alert('정상적으로 조회하지 못했습니다. ' + error)
       })
+  },
+  methods: {
+    showModal (storyId) {
+      this.isModalVisible = true
+      this.$emit('show-modal', storyId)
+    },
+    closeModal () {
+      this.isModalVisible = false
+    }
   }
 }
 </script>
