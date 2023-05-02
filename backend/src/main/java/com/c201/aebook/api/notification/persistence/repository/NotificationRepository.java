4e23a603612ca208aff2d31fcaebe3dae90f513c
한선영 <gyeoul98@naver.com>
2023-05-02 23:49:27 +0900

    feat: repository 내 알림 신청한 책 목록과 알림 상세 조회 쿼리문 구현
    repository 내 알림 신청한 책 목록과 알림 상세 조회를 하는 쿼리문을 구현하였습니다.
    S08P31C201-503
    
    
    Former-commit-id: f47d50c78ecd120dfa41477552cc86fc7752aadd

--- backend/src/main/java/com/c201/aebook/api/notification/persistence/repository/NotificationRepository.java
+++ backend/src/main/java/com/c201/aebook/api/notification/persistence/repository/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.c201.aebook.api.notification.persistence.repository;
 
 import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,11 @@ import java.util.Optional;
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
 
     NotificationEntity findByUserIdAndBookId(Long userId, Long bookId);
+
+    @Query(value = "SELECT n FROM NotificationEntity n LEFT JOIN FETCH n.user LEFT JOIN FETCH  n.book WHERE n.user.id = :userId",
+            countQuery = "select count(n) from NotificationEntity n WHERE n.user.id = :userId")
+    Page<NotificationEntity> findByUserId(Long userId, Pageable pageable);
+
+    @Query("SELECT n FROM NotificationEntity n LEFT JOIN FETCH n.user LEFT JOIN FETCH n.book WHERE n.id = :notificationId")
+    Optional<NotificationEntity> findByNotificationId(Long notificationId);
 }
