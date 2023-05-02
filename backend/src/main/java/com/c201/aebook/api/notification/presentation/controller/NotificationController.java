4abeba202aef3450c90cc2d70e6d71689cb4e080
한선영 <gyeoul98@naver.com>
2023-05-02 15:26:49 +0900

    feat: NotificationController 내 알림 신청 기능 구현
    NotificationController 내 saveNotification라는 알림 신청 기능을 구현하였습니다.
    S08P31C201-494
    
    
    Former-commit-id: eaaecc16b49bbf86b6d4dd8c4e4d82ccca1ee7bd

--- backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/NotificationController.java
+++ backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/NotificationController.java
@@ -1,4 +1,53 @@
 package com.c201.aebook.api.notification.presentation.controller;
 
+import com.c201.aebook.api.common.BaseResponse;
+import com.c201.aebook.api.notification.presentation.dto.request.NotificationRequestDTO;
+import com.c201.aebook.api.notification.presentation.vaildator.NotificationValidator;
+import com.c201.aebook.api.notification.service.impl.NotificationServiceImpl;
+import com.c201.aebook.api.vo.NotificationSO;
+import com.c201.aebook.auth.CustomUserDetails;
+import com.c201.aebook.converter.NotificationConverter;
+import com.c201.aebook.utils.RegexValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name="알림톡")
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/notification")
 public class NotificationController {
+
+    private final NotificationValidator notificationValidator;
+    private final RegexValidator regexValidator;
+    private final NotificationConverter notificationConverter;
+    private final NotificationServiceImpl notificationService;
+
+    @Operation(summary = "알림 신청", description = "사용자가 하나의 책에 알림 신청을 합니다.")
+    @SecurityRequirement(name = "bearerAuth")
+    @PostMapping()
+    public BaseResponse<?> saveNotification(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestBody NotificationRequestDTO notificationRequestDTO
+
+    ) {
+        // 1. DTO NOT NULL 검증
+        notificationValidator.validateNotificationRequestDTO(notificationRequestDTO);
+
+        // 2. isbn 검증
+        regexValidator.validateIsbn(notificationRequestDTO.getIsbn());
+
+        // 3. 알림 등록
+        NotificationSO notificationSO = notificationConverter.toNotificationSO(notificationRequestDTO);
+        notificationService.saveNotification(customUserDetails.getUsername(), notificationSO);
+
+        return new BaseResponse<>(null, HttpStatus.OK.value(), "알림 신청 성공");
+    }
+
 }
