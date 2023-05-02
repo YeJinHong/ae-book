83796c3e5f0288cff06651073045cb5d5accd605
한선영 <gyeoul98@naver.com>
2023-05-02 10:04:12 +0900

    feat: footer 내용 변경
    카카오 비즈니스 신청을 위해서 급하게 넣어서 merge 신청합니다...
    S08P31C201-409
    
    
    Former-commit-id: 0a25db614060f129e326b26c15019bf240b28c41

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -5,7 +5,8 @@
       <img src="../assets/images/aebook_logo.png">
       <div class="footer-text">
         <span>&copy;아이북(aebook) C201</span><br>
-        <span>상호명: 아이북 | 사업자 등록 번호: 746-43-01140 | 사업자명: 이도연</span>
+        <span>상호명: 아이북 | 사업자 등록 번호: 746-43-01140 | 사업자명: 이도연</span><br>
+        <span>주소: 광주광역시 남구 서문대로678번길 7, 103동 402호</span>
       </div>
     </div>
   </div>
