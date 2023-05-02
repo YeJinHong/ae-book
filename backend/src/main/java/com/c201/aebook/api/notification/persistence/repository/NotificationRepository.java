c4385119a339c0c0d8e010ab2006ef38b58a2998
한선영 <gyeoul98@naver.com>
2023-05-02 16:16:26 +0900

    feat: findByUserIdAndBookId의 return 타입 변경
    findByUserIdAndBookId의 return 타입을 NotificationEntity으로 변경하였습니다.
    S08P31C201-494
    
    
    Former-commit-id: acb0eddb0e31ed6eb7beb86669aee50e15083a63

--- backend/src/main/java/com/c201/aebook/api/notification/persistence/repository/NotificationRepository.java
+++ backend/src/main/java/com/c201/aebook/api/notification/persistence/repository/NotificationRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
 
-    Optional<NotificationEntity> findByUserIdAndBookId(Long userId, Long bookId);
+    NotificationEntity findByUserIdAndBookId(Long userId, Long bookId);
 }
