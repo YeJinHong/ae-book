b2aad56ff6de12c3dac31bf8f5fdeab4575500c6
한선영 <gyeoul98@naver.com>
2023-05-02 15:28:10 +0900

    feat: NotificationRepository 내 사용자Id와 책Id로 알림 찾기 기능 구현
    NotificationRepository 내 사용자Id와 책Id로 저장된 알림 찾기 기능을 구현하였습니다.
    S08P31C201-494
    
    
    Former-commit-id: c45b5d2ec157e015a3d96ecba72976b7b53b4004

--- backend/src/main/java/com/c201/aebook/api/notification/persistence/repository/NotificationRepository.java
+++ backend/src/main/java/com/c201/aebook/api/notification/persistence/repository/NotificationRepository.java
@@ -4,6 +4,10 @@ import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+
+    Optional<NotificationEntity> findByUserIdAndBookId(Long userId, Long bookId);
 }
