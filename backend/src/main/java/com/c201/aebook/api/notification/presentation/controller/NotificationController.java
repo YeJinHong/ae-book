cd764f1fdc7805764f82dd90a0226c3c0364aa6f
한선영 <gyeoul98@naver.com>
2023-05-03 10:20:21 +0900

    feat: controller 내 알림 수정 기능 구현
    controller 내에서 알림을 수정하는 기능을 구현하였습니다.
    S08P31C201-497
    
    
    Former-commit-id: eb7db71d7641d6ed89ac35cc54f485d14c27bd9a

--- backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/NotificationController.java
+++ backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/NotificationController.java
@@ -3,10 +3,13 @@ package com.c201.aebook.api.notification.presentation.controller;
 import com.c201.aebook.api.common.BaseResponse;
 import com.c201.aebook.api.common.constants.ApplicationConstants;
 import com.c201.aebook.api.notification.presentation.dto.request.NotificationRequestDTO;
+import com.c201.aebook.api.notification.presentation.dto.request.NotificationUpdateRequestDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookDetailResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookListResponseDTO;
+import com.c201.aebook.api.notification.presentation.dto.response.NotificationUpdateResponseDTO;
 import com.c201.aebook.api.notification.presentation.vaildator.NotificationValidator;
 import com.c201.aebook.api.notification.service.impl.NotificationServiceImpl;
+import com.c201.aebook.api.vo.NotificationPatchSO;
 import com.c201.aebook.api.vo.NotificationSO;
 import com.c201.aebook.auth.CustomUserDetails;
 import com.c201.aebook.converter.NotificationConverter;
@@ -79,6 +82,25 @@ public class NotificationController {
         // 1. 사용자가 신청한 알림에 대한 상세조회(알림 자체 정보와 책 정보를 결합)
         NotificationBookDetailResponseDTO notificationBookDetail = notificationService.getMyNotificationBookDetail(notificationId);
         return new BaseResponse<>(notificationBookDetail, HttpStatus.OK.value(), ApplicationConstants.SUCCESS);
+    }
+
+    @Operation(summary = "알림 수정", description = "사용자가 신청한 알림을 수정합니다.")
+    @SecurityRequirement(name = "bearerAuth")
+    @PatchMapping("/{notificationId}")
+    public BaseResponse<?> updateNotification(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable(name = "notificationId") Long notificationId,
+            @RequestBody NotificationUpdateRequestDTO notificationUpdateRequestDTO
+    ) {
+        // 1. DTO NOT NULL 검증
+        notificationValidator.validateNotificationUpdateRequestDTO(notificationUpdateRequestDTO);
+
+        // 2. 알림 수정
+        NotificationPatchSO notificationPatchSO = notificationConverter.toNotificationPatchSO(notificationUpdateRequestDTO);
+        NotificationUpdateResponseDTO notificationUpdateResponseDTO =
+                notificationService.updateNotification(customUserDetails.getUsername(), notificationId, notificationPatchSO);
+
+        return new BaseResponse<>(notificationUpdateResponseDTO, HttpStatus.OK.value(), ApplicationConstants.SUCCESS);
     }
 
 }
