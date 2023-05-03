da22572f4c360a7a03454925542dfb219e64840b
한선영 <gyeoul98@naver.com>
2023-05-03 15:53:44 +0900

    feat: 알림 신청 모달 생성
    알림 신청 모달을 생성하였습니다. 다만 form에서 sumbit을 하는 문제가 생겨서 현재는 사용하지 않습니다. 이 문제를 해결하면 이 모달로 변경할 예정입니다.
    S08P31C201-496
    
    
    Former-commit-id: 80b4bfd6781b50d7a28d424264fb4bd7b1d8bb7e

---
+++ frontend/src/components/notification/NotificationCreateModalButton.vue
@@ -0,0 +1,44 @@
+<template #modal-footer>
+  <div class="modal-footer">
+    <b-button class="ae-btn" @click="closeNotificationModal">닫기</b-button>
+    <b-button class="ae-btn btn-red" @click="saveUserNotification">신청</b-button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import notificationStore from '../../store/modules/notificationStore'
+
+export default {
+  name: 'NotificationCreateModalButton',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    ...mapActions(notificationStore, ['saveNotification']),
+    closeNotificationModal () {
+      console.log('닫기')
+      this.$emit('close')
+    },
+    saveUserNotification () {
+      console.log('알림 저장')
+
+      // const payload = {
+      //   data: {
+      //     isbn: this.isbn,
+      //     price: this.price
+      //   }
+      // }
+
+      // this.saveNotification()
+      this.closeNotificationModal()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
