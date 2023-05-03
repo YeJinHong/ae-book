51b0b95e4f29b8d22a1e8d21a8a8ffda0665c974
이해민 <2haemin98@gmail.com>
2023-05-03 14:46:36 +0900

    feat : header navbar css 추가
    메인 말고 header navbar 추가
    S08P31C201-492
    
    
    Former-commit-id: e4b56e803627290aba16f2b9f7e2f387f95bf11f

--- frontend/src/components/HeaderNavbar.vue
+++ frontend/src/components/HeaderNavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar toggleable="lg" id="headerNav">
             <b-navbar-brand href="#">
                 <router-link to="/">
                   <img src="../assets/images/aebook_logo.png" width="100" height="50">
@@ -10,25 +10,33 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
+            <b-navbar-nav id="navbarNav">
                 <b-nav-item>
                     <router-link to="/story">
-                            동화책 만들기
+                      <div class="nav-menu first">
+                        <b-nav-text>동화책 만들기</b-nav-text>
+                      </div>
                     </router-link>
                 </b-nav-item>
                 <b-nav-item>
                     <router-link to="/painting">
-                            색칠공부
+                      <div class="nav-menu second">
+                        <b-nav-text>색칠공부</b-nav-text>
+                      </div>
                     </router-link>
                 </b-nav-item>
                 <b-nav-item>
                     <router-link to="/book/searchbypicture">
-                            사진으로 책 검색
+                      <div class="nav-menu third">
+                        <b-nav-text>사진으로 책 검색</b-nav-text>
+                      </div>
                     </router-link>
                 </b-nav-item>
                 <b-nav-item>
                     <router-link to="/book/search">
-                            제목으로 책 검색
+                      <div class="nav-menu four">
+                        <b-nav-text>제목으로 책 검색</b-nav-text>
+                      </div>
                     </router-link>
                 </b-nav-item>
             </b-navbar-nav>
@@ -36,14 +44,15 @@
             <b-navbar-nav class="ml-auto" v-if=isLoginUser>
                     <b-nav-form>
                         {{ user.userId }} | {{ user.nickname }}
-                        <b-button size="sm" @click="logout">로그아웃</b-button>
-                        <router-link :to="{ name: 'MyInfo' }" class="link"><b-button size="sm">마이페이지</b-button></router-link>
+                        <b-button size="sm" @click="logout" class="btn-red">로그아웃</b-button>
+                        <router-link :to="{ name: 'MyInfo' }" class="link">
+                          <b-button size="sm" class="btn-navy">마이페이지</b-button></router-link>
                     </b-nav-form>
             </b-navbar-nav>
             <!-- 로그인 전 -->
             <b-navbar-nav class="ml-auto" v-else>
                 <b-nav-form>
-                    <router-link :to="{ name: 'Login' }" class="link"><b-button size="sm">로그인</b-button></router-link>
+                    <router-link :to="{ name: 'Login' }" class="link"><b-button size="sm" class="btn-red">로그인</b-button></router-link>
                 </b-nav-form>
             </b-navbar-nav>
             </b-collapse>
@@ -88,5 +97,35 @@ export default {
 </script>
 
 <style scoped>
+.nav-menu{
+  width: 200px;
+  height: 50px;
+  background-color: red;
+  border-radius: 30px 30px 0px 0px;
+  border: 10px solid white;
+  border-bottom: 0px;
+  position:relative;
+  padding: 0px;
+  text-align: center;
+}
 
+#navbarNav{
+  margin-bottom: -24px;
+}
+
+.nav-menu.first {
+  background-color: var(--menu-blue);
+}
+
+.nav-menu.second {
+  background-color: var(--menu-red);
+}
+
+.nav-menu.third {
+  background-color: var(--menu-yellow);
+}
+
+.nav-menu.fourth {
+  background-color: var(--menu-green);
+}
 </style>
