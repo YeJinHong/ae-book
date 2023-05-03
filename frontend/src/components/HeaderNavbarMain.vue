054bfc5777c8651b7d8b0fb969d2647a6389bcf4
이해민 <2haemin98@gmail.com>
2023-05-03 16:49:24 +0900

    feat : main header css 추가
    background image, button css 추가
    S08P31C201-487
    
    
    Former-commit-id: 00af11349cfe203123f267dfbffd24482f37764e

--- frontend/src/components/HeaderNavbarMain.vue
+++ frontend/src/components/HeaderNavbarMain.vue
@@ -10,40 +10,18 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item>
-                        <router-link to="/story">
-                                동화책 만들기
-                        </router-link>
-                    </b-nav-item>
-                    <b-nav-item>
-                        <router-link to="/painting">
-                                색칠공부
-                        </router-link>
-                    </b-nav-item>
-                     <b-nav-item>
-                    <router-link to="/book/searchbypicture">
-                            사진으로 책 검색
-                    </router-link>
-                </b-nav-item>
-                <b-nav-item>
-                    <router-link to="/book/search">
-                            제목으로 책 검색
-                    </router-link>
-                </b-nav-item>
-                </b-navbar-nav>
                 <!-- 로그인 후, 보여주는 정보는 추후 다시 결정, 마이페이지는 임시 배치 -->
                 <b-navbar-nav class="ml-auto" v-if=isLoginUser>
                     <b-nav-form>
                         {{ user.userId }} | {{ user.nickname }}
-                        <b-button size="sm" @click="logout">로그아웃</b-button>
-                        <router-link :to="{ name: 'MyInfo' }" class="link"><b-button size="sm">마이페이지</b-button></router-link>
+                        <b-button size="sm" @click="logout" class="btn-white"><img src="https://img.icons8.com/material-rounded/24/null/person-male.png"/>로그아웃</b-button>
+                        <router-link :to="{ name: 'MyInfo' }" class="link"><b-button size="sm" class="btn-white">마이페이지</b-button></router-link>
                     </b-nav-form>
                 </b-navbar-nav>
                 <!-- 로그인 전 -->
                 <b-navbar-nav class="ml-auto" v-else>
                     <b-nav-form>
-                        <router-link :to="{ name: 'Login' }" class="link"><b-button size="sm">로그인</b-button></router-link>
+                        <router-link :to="{ name: 'Login' }" class="link"><b-button size="sm" class="btn-white"><img src="https://img.icons8.com/material-rounded/24/null/person-male.png"/>로그인</b-button></router-link>
                     </b-nav-form>
                 </b-navbar-nav>
             </b-collapse>
@@ -88,5 +66,8 @@ export default {
 </script>
 
 <style scoped>
-
+.navbar{
+  height: 150px;
+  background-image: url("../assets/images/main_header.png");
+}
 </style>
