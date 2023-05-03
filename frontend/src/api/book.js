7de6220d6a6153c128c7de681a985af0ea27f378
leedoyeon <ldy__0202@naver.com>
2023-05-03 14:09:23 +0900

    feat: 메인 화면에 새로 들어온 책 조회
    S08P31C201-482
    
    
    Former-commit-id: b3ba22afbf28ea14b3e011a619c88a0de42c1c87

--- frontend/src/api/book.js
+++ frontend/src/api/book.js
@@ -3,4 +3,5 @@ import axios from 'axios'
 const searchByISBN = isbn => axios.get(`/api/books/${isbn}`)
 const getSearchList = (request) => axios.get(`/api/books?keyword=${request.keyword}&searchTargets=${request.searchTargets}&page=${request.page}`)
 const getISBNfromPicture = formData => axios.post(`/fast/books/isbn`, formData, {headers: {'Content-Type': 'multipart/form-data'}})
-export { searchByISBN, getSearchList, getISBNfromPicture }
+const getNewBookList = request => axios.get(`/api/books/new`)
+export { searchByISBN, getSearchList, getISBNfromPicture, getNewBookList }
--- frontend/src/views/AppMain.vue
+++ frontend/src/views/AppMain.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
     <header-navbar-main></header-navbar-main>
-    <div>아동 도서 콘텐츠 서비스</div>
+    <!-- 메인 메뉴 -->
+    <div class="menu-container">
+      <!-- <router-link :to="{ name: '' }"> -->
+        <div class="main-menu first">
+          <div class="menu-text">내가<br>만드는<br>동화</div>
+        </div>
+      <!-- </router-link> -->
+      <router-link :to="{ name: 'Painting' }">
+        <div class="main-menu second">
+          <div class="menu-text">색칠공부</div>
+        </div>
+      </router-link>
+      <router-link :to="{ name: 'BookSearchByPicture' }">
+        <div class="main-menu third">
+          <div class="menu-text">사진으로<br>도서 검색</div>
+        </div>
+      </router-link>
+      <router-link :to="{ name: 'BookSearch' }">
+        <div class="main-menu fourth">
+          <div class="menu-text">제목으로<br>도서 검색</div>
+        </div>
+      </router-link>
+    </div>
+    <!-- /메인 메뉴 -->
+    <div class="clear"></div>
+    <div class="subject-line"><div class="subject">새로 들어온 책</div><div class="red-bar"></div></div>
+    <div class="book-container">
+      <book-carousel-view :items="bookList" :chunkSize="4"></book-carousel-view>
+    </div>
     <!-- TODO : 각기 다음에 필요한 컴포넌트는 components/{domain} 하위에 작성 -->
     <!-- TODO : 새로 들어온 책 콘텐츠 케러셀 컴포넌트-->
+    <div class="subject-line"><div class="subject">최신 리뷰</div><div class="red-bar"></div></div>
     <carousel :items="reviewMainList"></carousel>
     <!-- TODO : 우리 아이들 작품(만든 동화) 리스트 컴포넌트 -->
+    <div class="subject-line"><div class="subject">우리 아이들 작품</div><div class="red-bar"></div></div>
     <the-footer></the-footer>
   </div>
 </template>
@@ -14,25 +44,31 @@
 import HeaderNavbarMain from '@/components/HeaderNavbarMain'
 import TheFooter from '@/components/TheFooter'
 import Carousel from '@/components/CarouselView'
+import BookCarouselView from '@/components/common/list/BookCarouselView.vue'
 import { mapState, mapActions } from 'vuex'
 
 const reviewStore = 'reviewStore'
+const bookStore = 'bookStore'
 
 export default {
   name: 'AppMain',
   components: {
     HeaderNavbarMain,
     TheFooter,
-    Carousel
+    Carousel,
+    BookCarouselView
   },
   methods: {
-    ...mapActions(reviewStore, ['getReviewMainListAction'])
+    ...mapActions(reviewStore, ['getReviewMainListAction']),
+    ...mapActions(bookStore, ['getNewBookList'])
   },
   computed: {
-    ...mapState(reviewStore, ['reviewMainList'])
+    ...mapState(reviewStore, ['reviewMainList']),
+    ...mapState(bookStore, ['bookList'])
   },
   mounted () {
     this.getReviewMainListAction()
+    this.getNewBookList()
   }
 }
 
@@ -53,5 +89,82 @@ export default {
   margin-bottom: 20px; /* 각 항목의 아래쪽 여백 */
   padding: 10px; /* 각 항목의 안쪽 여백 */
   border: 1px solid gray; /* 경계선 */
+}
+
+.book-container {
+  margin: auto;
+  height: 500px;
+  width: 1000px;
+}
+
+.menu-container{
+  width: 1000px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+}
+
+.main-menu{
+  width: 230px;
+  height: 340px;
+  background-color: red;
+  margin: 7px;
+  border-radius: 50px 10px 50px 10px;
+  border: 10px solid white;
+  position:relative;
+  text-align: left;
+}
+
+.main-menu.first {
+  background-color: var(--menu-blue);
+}
+
+.main-menu.second {
+  background-color: var(--menu-red);
+}
+
+.main-menu.third {
+  background-color: var(--menu-yellow);
+}
+
+.main-menu.fourth {
+  background-color: var(--menu-green);
+}
+
+.menu-text{
+  position: absolute;
+  color: white;
+  font-size: 32px;
+  font-weight: 800;
+  text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  margin: 30px;
+}
+
+.clear{
+  clear:both;
+}
+
+.subject{
+  text-align: left;
+  font-size: 24px;
+  font-weight: 800;
+  margin-right: 15px;
+}
+
+.subject-line{
+  width: 1000px;
+  margin: auto;
+  margin-top: 100px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+}
+
+.red-bar {
+  height:2px;
+  background-color: var(--ae-red);
+  width:141px;
 }
 </style>
