ae27b30e0b8cdd9b7439806343a72c67a12e52de
한선영 <gyeoul98@naver.com>
2023-05-03 22:31:19 +0900

    feat: ResponseDTO 내 알림타입 추가
    ResponseDTO 내 알림타입을 추가하였습니다.
    S08P31C201-497
    
    
    Former-commit-id: 96589a64413d22438e804540d0a0e31445c9d2b3

--- backend/src/main/java/com/c201/aebook/api/notification/presentation/dto/request/NotificationUpdateRequestDTO.java
+++ backend/src/main/java/com/c201/aebook/api/notification/presentation/dto/request/NotificationUpdateRequestDTO.java
@@ -15,4 +15,11 @@ public class NotificationUpdateRequestDTO {
     @Schema(description = "금액 상한선", defaultValue = "5000")
     private int upperLimit;
 
+    /**
+     * S는 사용자가 가격을 지정한 경우(selected),
+     * D는 사용자가 가격을 지정하지 않은 경우(default)
+     */
+    @Schema(description = "알림 타입", defaultValue = "S")
+    private String notificationType;
+
 }
