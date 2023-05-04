70ef260d53240e90f7de47edf43daf54c34eda12
한선영 <gyeoul98@naver.com>
2023-05-04 09:32:31 +0900

    style: change에서 변경된 사항 다시 변경
    change에서 변경된 사항 다시 변경하였습니다.
    S08P31C201-496
    
    
    Former-commit-id: 8b173c1d5a280d7b2c975e8ce3f69732d699213e

--- backend/src/main/java/com/c201/aebook/converter/NotificationConverter.java
+++ backend/src/main/java/com/c201/aebook/converter/NotificationConverter.java
@@ -3,6 +3,7 @@ package com.c201.aebook.converter;
 import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
 import com.c201.aebook.api.notification.presentation.dto.request.NotificationRequestDTO;
 import com.c201.aebook.api.notification.presentation.dto.request.NotificationUpdateRequestDTO;
+import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookDetailResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookListResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationUpdateResponseDTO;
 import com.c201.aebook.api.vo.NotificationPatchSO;
