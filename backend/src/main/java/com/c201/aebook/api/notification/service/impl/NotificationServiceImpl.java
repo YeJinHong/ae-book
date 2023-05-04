e5d64fb9d69ca43471636df3a8de507fb03afcc2
한선영 <gyeoul98@naver.com>
2023-05-04 09:50:38 +0900

    feat: service 내 saveNotification의 return 타입 변경
    service 내 saveNotification의 return 타입을 NotificationResponseDTO으로 변경하였습니다.
    S08P31C201-494
    
    
    Former-commit-id: d57011d1c0c93f9e2e5d8c3958db59c31b7c5758

--- backend/src/main/java/com/c201/aebook/api/notification/service/impl/NotificationServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/notification/service/impl/NotificationServiceImpl.java
@@ -6,6 +6,7 @@ import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
 import com.c201.aebook.api.notification.persistence.repository.NotificationRepository;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookDetailResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookListResponseDTO;
+import com.c201.aebook.api.notification.presentation.dto.response.NotificationResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationUpdateResponseDTO;
 import com.c201.aebook.api.notification.service.NotificationService;
 import com.c201.aebook.api.user.persistence.entity.UserEntity;
@@ -34,7 +35,7 @@ public class NotificationServiceImpl implements NotificationService {
     private final NotificationConverter notificationConverter;
 
     @Override
-    public void saveNotification(String userId, NotificationSO notificationSO) {
+    public NotificationResponseDTO saveNotification(String userId, NotificationSO notificationSO) {
         // 1. isbn 유효성 검증
         BookEntity bookEntity = bookRepository.findByIsbn(notificationSO.getIsbn())
                 .orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
@@ -56,13 +57,16 @@ public class NotificationServiceImpl implements NotificationService {
             throw new CustomException(ErrorCode.DUPLICATED_NOTIFICATION);
         }
 
-        // 5. 알림 신청 저장
-        notificationRepository.save(NotificationEntity.builder()
+        // 4. 알림 신청 저장
+        NotificationEntity notification = notificationRepository.save(NotificationEntity.builder()
                 .upperLimit(notificationSO.getUpperLimit())
                 .notificationType(notificationSO.getNotificationType())
                 .user(userEntity)
                 .book(bookEntity)
                 .build());
+
+        NotificationResponseDTO notificationResponseDTO = notificationConverter.toNotificationResponseDTO(notification);
+        return notificationResponseDTO;
     }
 
     @Override
