085904fc02874a78ab9b283152971ec9d745b9d4
이해민 <2haemin98@gmail.com>
2023-05-04 15:47:28 +0900

    style : footer 주소 정보 삭제
    footer에 사업자 주소 삭제
    S08P31C201-493
    
    
    Former-commit-id: e3e9e39cd767ea6815b90fb88fd8b7c8bd43ac20

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -6,7 +6,6 @@
       <div class="footer-text">
         <span>&copy;아이북(aebook) C201</span><br>
         <span>상호명: 아이북 | 사업자 등록 번호: 746-43-01140 | 사업자명: 이도연</span><br>
-        <span>주소: 광주광역시 남구 서문대로678번길 7, 103동 402호</span><br>
         <span>도서 API 제공 : 알라딘 인터넷서점( www.aladin.co.kr)</span>
       </div>
     </div>
