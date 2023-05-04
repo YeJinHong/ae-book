54ac235265304b9731ae81a92c9c0e6fcad9b507
leedoyeon <ldy__0202@naver.com>
2023-05-04 10:59:54 +0900

    feat: 그림판 화면
    S08P31C201-358
    
    
    Former-commit-id: 2fbaa67bbb14263766da553bb4ce5d92ab2a9b43

--- frontend/src/components/painting/PaintingBoardView.vue
+++ frontend/src/components/painting/PaintingBoardView.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="container">
-    <input type="text" v-model="title" placeholder="제목을 꼭">
-    <canvas id="canvas"
-      ref="canvas"
-      @mousemove="onMove"
-      @mousedown="startPainting"
-      @mouseup="cancelPainting"
-      @mouseleave="cancelPainting"
-      @touchmove="onMove"
-      @touchstart="startPainting"
-      @touchend="cancelPainting"
-      @touchcancel ="cancelPainting"
-    ></canvas>
+    <!-- 왼쪽 -->
+    <div class="container-left">
+      <label class="title">제목 : </label>
+      <input type="text" v-model="title">
+      <canvas id="canvas"
+        ref="canvas"
+        @mousemove="onMove"
+        @mousedown="startPainting"
+        @mouseup="cancelPainting"
+        @mouseleave="cancelPainting"
+        @touchmove="onMove"
+        @touchstart="startPainting"
+        @touchend="cancelPainting"
+        @touchcancel ="cancelPainting"
+      ></canvas>
+    </div>
+    <!-- 왼쪽 -->
+    <!-- 오른쪽 -->
+    <div class="container-right">
     <div id="palette">
       <div class="left">
         <div
@@ -39,9 +46,11 @@
       <button id="brush-btn" @click="onBrushClick">브러쉬</button>
     </div>
     <div>
-      <button>종료</button>
-      <button @click="onSaveClick">저장</button>
+      <button class="ae-btn btn-navy">종료</button>
+      <button @click="onSaveClick" class="ae-btn btn-red">저장</button>
     </div>
+  </div>
+  <!-- 오른쪽 -->
   </div>
 </template>
 
@@ -69,13 +78,13 @@ export default {
   mounted () {
     this.canvas = this.$refs.canvas
     this.ctx = this.canvas.getContext('2d')
-    this.canvas.width = 800
+    this.canvas.width = 700
     this.canvas.height = 500
     this.ctx.lineWidth = 10
     // base64 문자열 이미지 로드
     const image = new Image()
     image.src = this.sketch
-    this.ctx.drawImage(image, 0, 0, 800, 500)
+    this.ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height)
   },
   methods: {
     ...mapActions(paintingStore, ['savePainting']),
@@ -143,6 +152,11 @@ export default {
       return paintingFile
     },
     onSaveClick () {
+      // 제목을 썼는지 체크
+      if (this.title.trim() === '') {
+        alert('제목을 입력해주세요.')
+        return
+      }
       const paintingFile = this.canvasToFile(this.canvas)
 
       let data = {
@@ -168,12 +182,26 @@ export default {
 <style scoped>
 .container {
   padding: 0 100px;
+  display: flex;
+  flex-direction: row;
+}
+
+.container-left {
+  width: 80%;
+  margin-right: 30px;
+}
+
+.container-right {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 }
 
 #canvas {
     border-radius: 15px;
     box-shadow: 0px 1px 10px 0.1px rgba(0, 0, 0, 0.3);
-    width: 800px;
+    width: 700px;
     height:  500px;
     float: left;
 }
@@ -182,8 +210,7 @@ export default {
 #palette {
   display: flex;
   flex-direction: row;
-  /* background-color: pink; */
-  width: 200px;
+  justify-content: center;
 }
 
 .left, .right {
@@ -198,6 +225,11 @@ export default {
   box-shadow: 1px 3px 3px 0.1px rgba(0, 0, 0, 0.3);
   border-radius: 50%;
   cursor: pointer;
+}
+
+.title {
+  font-size: 35px;
+  font-weight: 800;
 }
 
 </style>
