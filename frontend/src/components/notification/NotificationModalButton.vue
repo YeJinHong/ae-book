8caf3259f2ff8cd7ef1506a959399af8798326f7
한선영 <gyeoul98@naver.com>
2023-05-04 17:52:10 +0900

    feat: Notification 모달에 사용되는 버튼 컨포넌트 생성
    Notification 모달에 사용되는 버튼 컨포넌트 생성하였습니다. 아직 기능 작동은 안됩니다.
    S08P31C201-505
    
    
    Former-commit-id: 8437efb40206ae16b1d323cf229b4265ac467668

---
+++ frontend/src/components/notification/NotificationModalButton.vue
@@ -0,0 +1,30 @@
+<template #modal-footer>
+  <div class="modal-footer">
+    <button class="ae-btn" @click="closeModal">닫기</button>
+  </div>
+</template>
+<script>
+import { mapActions, mapState } from 'vuex'
+const paintingStore = 'paintingStore'
+
+export default {
+  name: 'NotificationModalButton',
+  computed: {
+    ...mapState(paintingStore, ['painting'])
+  },
+  methods: {
+    ...mapActions('paintingStore', ['downloadPainting', 'deletePainting', 'updatePaintingTitle']),
+    closeModal () {
+      this.$emit('close')
+    },
+    clickDelete (id) {
+      this.deletePainting(id)
+      this.closeModal()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
