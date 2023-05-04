86725935ce3a831f5ee2b58e6e3b311b548dce88
이해민 <2haemin98@gmail.com>
2023-05-04 15:52:33 +0900

    style : footer logo 수정
    피그마 로고로 변경
    S08P31C201-493
    
    
    Former-commit-id: dd44197424afb106a9138a36ee862d37f9569aba

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -2,7 +2,9 @@
   <div>
     <div>&nbsp;</div>
     <div class="footer">
-      <img src="../assets/images/aebook_logo.png">
+      <div style="text-align: left">
+        <img src="../assets/images/main_logo.png">
+      </div>
       <div class="footer-text">
         <span>&copy;아이북(aebook) C201</span><br>
         <span>상호명: 아이북 | 사업자 등록 번호: 746-43-01140 | 사업자명: 이도연</span><br>
