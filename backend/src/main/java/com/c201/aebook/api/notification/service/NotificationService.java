dcddcacefbbef139c101815e2aa91aa1153c3637
한선영 <gyeoul98@naver.com>
2023-05-04 09:27:25 +0900

    feat: backend code 삭제
    브랜치를 잘못봐서 backend code를 삭제하겠습니다... 이 코드는 다른 브랜치에서 다시 올리겠습니다
    S08P31C201-
    
    
    Former-commit-id: 5b9a6c34dcc63c312c9e4fcfc5247f5ee7796c31

--- backend/src/main/java/com/c201/aebook/api/notification/service/NotificationService.java
+++ backend/src/main/java/com/c201/aebook/api/notification/service/NotificationService.java
@@ -2,7 +2,6 @@ package com.c201.aebook.api.notification.service;
 
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookDetailResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookListResponseDTO;
-import com.c201.aebook.api.notification.presentation.dto.response.NotificationResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationUpdateResponseDTO;
 import com.c201.aebook.api.vo.NotificationPatchSO;
 import com.c201.aebook.api.vo.NotificationSO;
@@ -11,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 
 public interface NotificationService {
 
-    public NotificationResponseDTO saveNotification(String userId, NotificationSO notificationSO);
+    public void saveNotification(String userId, NotificationSO notificationSO);
 
     public Page<NotificationBookListResponseDTO> getMyNotificationBookList(String userId, Pageable pageable);
 
--- backend/src/main/java/com/c201/aebook/converter/NotificationConverter.java
+++ backend/src/main/java/com/c201/aebook/converter/NotificationConverter.java
@@ -3,9 +3,7 @@ package com.c201.aebook.converter;
 import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
 import com.c201.aebook.api.notification.presentation.dto.request.NotificationRequestDTO;
 import com.c201.aebook.api.notification.presentation.dto.request.NotificationUpdateRequestDTO;
-import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookDetailResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationBookListResponseDTO;
-import com.c201.aebook.api.notification.presentation.dto.response.NotificationResponseDTO;
 import com.c201.aebook.api.notification.presentation.dto.response.NotificationUpdateResponseDTO;
 import com.c201.aebook.api.vo.NotificationPatchSO;
 import com.c201.aebook.api.vo.NotificationSO;
@@ -19,7 +17,5 @@ public interface NotificationConverter {
 
     NotificationPatchSO toNotificationPatchSO(NotificationUpdateRequestDTO notificationUpdateRequestDTO);
     NotificationUpdateResponseDTO toNotificationUpdateResponseDTO(Integer upperLimit, String notificationType);
-
-    NotificationResponseDTO toNotificationResponseDTO(NotificationEntity notificationEntity);
 
 }
