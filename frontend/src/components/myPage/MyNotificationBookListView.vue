1b5ae61e5838e32cfac2e56edbb948e495d343f4
한선영 <gyeoul98@naver.com>
2023-05-04 17:50:27 +0900

    feat: 알림 신청한 책 목록 컨포넌트 생성 및 기능 구현
    알림 신청한 책 목록 컨포넌트 생성 및 기능을 구현하였습니다. 다만 아직 알림 신청한 책 상세보기는 불가능합니다.
    S08P31C201-505
    
    
    Former-commit-id: cdd5b8b774405312c5fef5ba30928d44f873c7ac

---
+++ frontend/src/components/myPage/MyNotificationBookListView.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1 class="subject">알림 신청한 책 목록</h1>
+    <div style="height:2px; background-color: #E0E0E0;"></div>
+    <ModalView :modalShow="isModalVisible" @close-modal="closeModal">
+      <notification-detail-view/>
+      <notification-modal-button @close="closeModal"></notification-modal-button>
+    </ModalView>
+    <div class="carousel-container">
+      <img-carousel-view :items="notificationList" :chunkSize="3" @moveTo="showModal"></img-carousel-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+import ModalView from '@/components/common/ModalView.vue'
+import ImgCarouselView from '@/components/common/list/ImgCarouselView.vue'
+import NotificationDetailView from '../notification/NotificationDetailView.vue'
+import NotificationModalButton from '../notification/NotificationModalButton.vue'
+const notificationStore = 'notificationStore'
+
+export default {
+  name: 'MyNotificationBookListView',
+  components: {
+    ModalView,
+    ImgCarouselView,
+    NotificationDetailView,
+    NotificationModalButton
+  },
+  data () {
+    return {
+      isModalVisible: false
+    }
+  },
+  computed: {
+    ...mapState(notificationStore, ['notificationList'])
+  },
+  mounted () {
+    this.getBookNotificationList()
+    console.log(this.notificationList)
+  },
+  methods: {
+    ...mapActions(notificationStore, ['getBookNotificationList', 'getNotificationDetail']),
+    showModal (notificationId) {
+      this.getNotificationDetail(notificationId)
+      this.isModalVisible = true
+    },
+    closeModal () {
+      this.isModalVisible = false
+      // this.clearStoryId()
+    }
+  }
+
+}
+</script>
+
+<style>
+.subject{
+  text-align: left;
+  font-size: 24px;
+  font-weight: 800;
+}
+</style>
