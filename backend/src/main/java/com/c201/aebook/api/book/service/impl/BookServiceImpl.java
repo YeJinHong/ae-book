bea06d6fc80b2d9433f1aa72ba03af7977785a05
한선영 <gyeoul98@naver.com>
2023-05-04 14:53:49 +0900

    Revert "feat: BookDetail에서 알림ID 값 추가를 위해 로직 변경"
    This reverts commit ae27edf6948a88befffc375d06f0ebb8b12026d0 [formerly 7396c44c8f5e6df47817a7e9e0900867aadd393f].
    
    
    Former-commit-id: 0ebff58abcab44f2fb9987bc52712d798f62d916

--- backend/src/main/java/com/c201/aebook/api/book/service/impl/BookServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/book/service/impl/BookServiceImpl.java
@@ -3,7 +3,6 @@ package com.c201.aebook.api.book.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
 import com.c201.aebook.api.notification.persistence.repository.NotificationRepository;
 import com.c201.aebook.auth.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
@@ -39,18 +38,14 @@ public class BookServiceImpl implements BookService {
 			.orElseThrow(() -> new CustomException(ErrorCode.BOOK_NOT_FOUND));
 		BookResponseDTO bookResponseDTO = bookConverter.toBookResponseDTO(book);
 
-		bookResponseDTO.setNotification(false);
-
-		// 사용자 정보를 가지고 있는 경우
+		// 사용자 정보 찾기
+		boolean notitication = false;
 		if(customUserDetails != null) {
-			NotificationEntity notificationEntity = notificationRepository.findByUserIdAndBookId(Long.valueOf(customUserDetails.getUsername()), book.getId());
-
-			// notification 정보를 가지고 있는 경우
-			if (notificationEntity != null) {
-				bookResponseDTO.setNotification(true);
-				bookResponseDTO.setNotificationId(notificationEntity.getId());
-			}
+			// 사용자Id와 bookId로 알림 정보 찾기
+			notitication = notificationRepository.existsByBookId(book.getId(), Long.valueOf(customUserDetails.getUsername()));
 		}
+
+		bookResponseDTO.setNotification(notitication);
 
 		return bookResponseDTO;
 	}
@@ -65,11 +60,9 @@ public class BookServiceImpl implements BookService {
 	}
 
 	@Override
-	public Page<BookSearchResponseDTO> searchBookList(String keyword, String[] searchTarget, Pageable pageable, CustomUserDetails customUserDetails) {
+	public Page<BookSearchResponseDTO> searchBookList(String keyword, String[] searchTarget, Pageable pageable) {
 		Page<BookEntity> bookList = bookCustomRepository.searchBookList(keyword, searchTarget, pageable);
 		Page<BookSearchResponseDTO> result = bookList.map(book -> bookConverter.toBookSearchResponseDTO(book));
-
-
 		return result;
 	}
 
