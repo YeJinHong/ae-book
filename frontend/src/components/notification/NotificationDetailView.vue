212765798f4a1f6067ef2cefb8ccda819652ad6d
한선영 <gyeoul98@naver.com>
2023-05-04 17:51:26 +0900

    feat: 알림 신청한 책 상세보기 컨포넌트를 생성
    알림 신청한 책 상세보기 컨포넌트를 생성하였습니다. 다만 아직 기능 작동은 불가능합니다.
    S08P31C201-505
    
    
    Former-commit-id: c72c7bc3c5fb549c17bd0f567ae34faa92d73ad6

---
+++ frontend/src/components/notification/NotificationDetailView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" v-model="painting.title">
+    <button class="ae-btn btn-red" @click="updatePaintingTitle({paintingId: painting.id, title: {title:painting.title}})">수정</button>
+    <img v-bind:src="painting.fileUrl">
+  </div>
+  </template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+const paintingStore = 'paintingStore'
+
+export default {
+  name: 'NotificationDetailView',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapState(paintingStore, ['painting'])
+  },
+  methods: {
+    ...mapActions('paintingStore', ['updatePaintingTitle'])
+  }
+}
+</script>
+
+  <style>
+
+  </style>
