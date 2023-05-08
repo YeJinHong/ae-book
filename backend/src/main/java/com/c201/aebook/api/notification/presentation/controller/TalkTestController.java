bba7a9b15c9c2e08c91754ad7d1d0c79d0e0d097
한선영 <gyeoul98@naver.com>
2023-05-09 01:39:26 +0900

    feat: controller 내 사용자 정보로 사용자 지정 최저가 알림 보내기 기능 구현
    controller 내 사용자 정보로 사용자 지정 최저가 알림 보내기 기능을 구현하였습니다.  다만 책 정보는 배치에서 받아올 예정이라 임시로 bookId를 받아서 사용하였습니다.
    S08P31C201-461
    
    
    Former-commit-id: 83b7cf2bfb2c295bf38e2e16f9479e3417084802

--- backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/TalkTestController.java
+++ backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/TalkTestController.java
@@ -1,5 +1,6 @@
 package com.c201.aebook.api.notification.presentation.controller;
 
+import com.c201.aebook.api.notification.persistence.entity.NotificationEntity;
 import com.c201.aebook.api.notification.service.impl.TalkServiceImpl;
 import com.c201.aebook.api.user.persistence.entity.UserEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -36,6 +37,7 @@ public class TalkTestController {
         return talkService.createToken();
     }
 
+    // 최저가 알림톡 기본 형식
 //    @GetMapping("/lower-price")
 //    public ResponseEntity lowestPriceTalk() throws ParseException, JsonProcessingException {
 //        // 토큰 생성
@@ -48,24 +50,25 @@ public class TalkTestController {
 //        return ResponseEntity.ok().body(LowestPriceResponse);
 //    }
 
-    @GetMapping("/customize-lower-price")
-    public ResponseEntity customizeLowestPriceTalk() throws ParseException, JsonProcessingException {
-        // 토큰 생성
-        String token = talkService.createToken();
+    // 사용자 지정 최저가 알림톡 기본 형식
+//    @GetMapping("/customize-lower-price")
+//    public ResponseEntity customizeLowestPriceTalk() throws ParseException, JsonProcessingException {
+//        // 토큰 생성
+//        String token = talkService.createToken();
+//
+//        // 최저가 알림톡 탬플릿 만들어서 알림톡 전송
+//        // 결과코드(code)가 0이면 성공 -99이면 전송 실패
+//        ResponseEntity<String> customizeLowestPrice = talkService.CustomizeLowestPriceTalk(token);
+//
+//        return ResponseEntity.ok().body(customizeLowestPrice);
+//    }
 
-        // 최저가 알림톡 탬플릿 만들어서 알림톡 전송
-        // 결과코드(code)가 0이면 성공 -99이면 전송 실패
-        ResponseEntity<String> customizeLowestPrice = talkService.CustomizeLowestPriceTalk(token);
-
-        return ResponseEntity.ok().body(customizeLowestPrice);
-    }
-
-    @GetMapping("/book")
+    @GetMapping("/book-lower-price")
     public ResponseEntity bookLowestPriceTalk(
             @RequestParam(name = "bookId") Long bookId
     ) throws ParseException, JsonProcessingException {
         // 1. notificationRepository에서 bookId로 알림 정보랑 유저들 정보 찾기
-        List<UserEntity> userList = talkService.getNotificationUserInfoByBookId(bookId);
+        List<NotificationEntity> userList = talkService.getNotificationUserInfoByBookId(bookId);
 
         // 2. bookId로 book title 가져오기 -> 배치에서는 바로 가져올 수 있지만 현재는 불가능하므로 repository에서 가져오기
         String bookTitle = talkService.getBookTitle(bookId);
@@ -77,6 +80,33 @@ public class TalkTestController {
         ResponseEntity<String> LowestPriceResponse = talkService.LowestPriceTalk(token, userList, bookTitle);
 
         return ResponseEntity.status(200).body(LowestPriceResponse);
+    }
+
+    @GetMapping("/book-customize-lower-price")
+    public ResponseEntity bookCustomizeLowestPriceTalk(
+            @RequestParam(name = "bookId") Long bookId
+    ) throws ParseException, JsonProcessingException {
+        // 1. bookId로 book price 가져오기 -> 배치에서는 바로 가져올 수 있지만 현재는 불가능하므로 bookRepository에서 가져오기
+        int bookPrice = talkService.getBookPrice(bookId);
+
+        // 2. notificationRepository에서 bookId로 알림 정보랑 유저들 정보 찾기
+        List<NotificationEntity> userList = talkService.getNotificationUserInfoByBookId(bookId);
+
+        // 3. bookId로 book title 가져오기 -> 배치에서는 바로 가져올 수 있지만 현재는 불가능하므로 bookRepository에서 가져오기
+        String bookTitle = talkService.getBookTitle(bookId);
+
+        // 1번과 3번은 배치에서 bookEntity로 가져올 수도 있을듯?
+
+        // 4. token 정보 얻어오기
+        String token = talkService.createToken();
+
+        // 5. 유저 정보로 알림톡 맞춰서 보내서 결과 받기
+        ResponseEntity<String> customizeLowestPrice = talkService.CustomizeLowestPriceTalk(token, userList, bookTitle, bookPrice);
+        if(customizeLowestPrice == null) {
+            return ResponseEntity.status(200).body("전송 대상 없음");
+        }
+
+        return ResponseEntity.status(200).body(customizeLowestPrice);
     }
 
 }
