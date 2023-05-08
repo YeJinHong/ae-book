238e6bdb63076bc642f34f26712b7fdf25fe3310
한선영 <gyeoul98@naver.com>
2023-05-08 21:33:30 +0900

    feat: service 내 사용자 정보로 최저가 알림 보내기 기능 구현
    service 내 사용자 정보로 최저가 알림 보내기 기능 구현하였습니다. book 정보는 배치에서 받을 수 있지만 현재는 불가능하여 repository에서 가져다 사용했습니다. 여러명의 사용자에게 알림을 보내기 위해 for문을 사용하였습니다.
    S08P31C201-461
    
    
    Former-commit-id: b0fbddbef9cff0154ad18195550734a17d1ee377

--- backend/src/main/java/com/c201/aebook/api/notification/service/TalkService.java
+++ backend/src/main/java/com/c201/aebook/api/notification/service/TalkService.java
@@ -1,15 +1,21 @@
 package com.c201.aebook.api.notification.service;
 
+import com.c201.aebook.api.user.persistence.entity.UserEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface TalkService {
 
     public String createToken() throws JsonProcessingException, ParseException;
 
-    public ResponseEntity<String> LowestPriceTalk(String token);
+    public ResponseEntity<String> LowestPriceTalk(String token, List<UserEntity> userList, String bookTitle);
 
     public ResponseEntity<String> CustomizeLowestPriceTalk(String token);
 
+    public List<UserEntity> getNotificationUserInfoByBookId(Long bookId);
+
+    public String getBookTitle(Long bookId);
 }
