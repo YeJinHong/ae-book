3c41bfe72004724c90362f62f72082e083ce63bc
한선영 <gyeoul98@naver.com>
2023-05-08 21:30:54 +0900

    feat: controller 내 사용자 정보로 최저가 알림 보내기 기능 구현
    controller 내 사용자 정보로 최저가 알림 보내기 기능 구현하였습니다. 다만 책 정보는 배치에서 받아올 예정이라 임시로 bookId를 받아서 사용하였습니다.
    S08P31C201-461
    
    
    Former-commit-id: 7e27095dbaaf9c4d787e016dd0aa0c4b07257a0f

--- backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/TalkTestController.java
+++ backend/src/main/java/com/c201/aebook/api/notification/presentation/controller/TalkTestController.java
@@ -1,6 +1,7 @@
 package com.c201.aebook.api.notification.presentation.controller;
 
 import com.c201.aebook.api.notification.service.impl.TalkServiceImpl;
+import com.c201.aebook.api.user.persistence.entity.UserEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
@@ -35,17 +36,17 @@ public class TalkTestController {
         return talkService.createToken();
     }
 
-    @GetMapping("/lower-price")
-    public ResponseEntity lowestPriceTalk() throws ParseException, JsonProcessingException {
-        // 토큰 생성
-        String token = talkService.createToken();
-
-        // 최저가 알림톡 탬플릿 만들어서 알림톡 전송
-        // 결과코드(code)가 0이면 성공 -99이면 전송 실패
-        ResponseEntity<String> LowestPriceResponse = talkService.LowestPriceTalk(token);
-
-        return ResponseEntity.ok().body(LowestPriceResponse);
-    }
+//    @GetMapping("/lower-price")
+//    public ResponseEntity lowestPriceTalk() throws ParseException, JsonProcessingException {
+//        // 토큰 생성
+//        String token = talkService.createToken();
+//
+//        // 최저가 알림톡 탬플릿 만들어서 알림톡 전송
+//        // 결과코드(code)가 0이면 성공 -99이면 전송 실패
+//        ResponseEntity<String> LowestPriceResponse = talkService.LowestPriceTalk(token);
+//
+//        return ResponseEntity.ok().body(LowestPriceResponse);
+//    }
 
     @GetMapping("/customize-lower-price")
     public ResponseEntity customizeLowestPriceTalk() throws ParseException, JsonProcessingException {
@@ -57,6 +58,25 @@ public class TalkTestController {
         ResponseEntity<String> customizeLowestPrice = talkService.CustomizeLowestPriceTalk(token);
 
         return ResponseEntity.ok().body(customizeLowestPrice);
+    }
+
+    @GetMapping("/book")
+    public ResponseEntity bookLowestPriceTalk(
+            @RequestParam(name = "bookId") Long bookId
+    ) throws ParseException, JsonProcessingException {
+        // 1. notificationRepository에서 bookId로 알림 정보랑 유저들 정보 찾기
+        List<UserEntity> userList = talkService.getNotificationUserInfoByBookId(bookId);
+
+        // 2. bookId로 book title 가져오기 -> 배치에서는 바로 가져올 수 있지만 현재는 불가능하므로 repository에서 가져오기
+        String bookTitle = talkService.getBookTitle(bookId);
+
+        // 3. token 정보 얻어오기
+        String token = talkService.createToken();
+
+        // 4. 유저 정보로 알림톡 맞춰서 보내서 결과 받기
+        ResponseEntity<String> LowestPriceResponse = talkService.LowestPriceTalk(token, userList, bookTitle);
+
+        return ResponseEntity.status(200).body(LowestPriceResponse);
     }
 
 }
