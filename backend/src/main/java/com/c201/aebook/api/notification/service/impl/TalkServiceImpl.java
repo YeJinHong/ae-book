148437cd14bee50c50d4d6933e7aeaac96523e3b
한선영 <gyeoul98@naver.com>
2023-05-08 16:44:51 +0900

    feat: 도서 최저가 갱신 시 전송되는 테스트 알림톡 탬플릿 작성
    도서 최저가 갱신 시 전송되는 테스트 알림톡 탬플릿을 작성하였습니다. 현재는 테스트를 하기 위해 하드코딩으로 되어 있습니다. 배치로 이동한 후는 올바르게 작동하도록 변경하겠습니다.
    S08P31C201-564
    
    
    Former-commit-id: 29b4f55f5b11eddae6d4323e039e61c38c6c49ae

--- backend/src/main/java/com/c201/aebook/api/notification/service/impl/TalkServiceImpl.java
+++ backend/src/main/java/com/c201/aebook/api/notification/service/impl/TalkServiceImpl.java
@@ -79,4 +79,61 @@ public class TalkServiceImpl implements TalkService {
         return token;
     }
 
+    public ResponseEntity<String> LowestPriceTalk(String token) {
+        /**
+         * 알림톡 전송을 위해 정보 받아오기
+         * 일단 테스틀 위해 하드코딩
+         * */
+        String username = "도연";
+        String bookTitle = "자바와 JUnit을 활용한 실용주의 단위 테스트";
+        String subject = "도서 최저가 갱신";
+
+        //버튼 정보 입력
+        Map<String, String> buttonMap = new HashMap<>();
+        buttonMap.put("name", "아이북 바로가기"); // 버튼명
+        buttonMap.put("linkType", "WL"); // 버튼 링크타입(DS:배송조회, WL:웹링크, AL:앱링크, BK:봇키워드, MD:메시지전달)
+        buttonMap.put("linkTypeName", "웹링크"); // 버튼 링크 타입네임, ( 배송조회, 웹링크, 앱링크, 봇키워드, 메시지전달 중에서 1개)
+        buttonMap.put("linkM", aebookUrl); // WL일때 필수
+        buttonMap.put("linkP", aebookUrl); // WL일때 필수
+        List<Map> button = new ArrayList<>();
+        button.add(buttonMap);
+        Map<String, List<Map>> buttonInfo = new HashMap<>(); // 버튼 정보
+        buttonInfo.put("button", button);
+
+        // 전송 객체 생성
+        RestTemplate rt = new RestTemplate();
+
+        // header와 body 생성
+        HttpHeaders headers = new HttpHeaders();
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<String, Object>();
+        body.add("apikey", TalkApiKey); // api key
+        body.add("userid", TalkUserId); // 사이트 아이디
+        body.add("token", token); // 발급받은 토큰
+        body.add("senderkey", TalkSenderKey); // 발신프로파일 키
+        body.add("tpl_code", LowestPriceTplCode); // 템플릿 코드
+        body.add("sender", TalkSender); // 발신자 연락처
+
+        body.add("receiver_1", "01071276216"); // 수신자 연락처
+        body.add("subject_1", subject); // 알림톡 제목(발신자만 보임)
+        body.add("message_1", username + " 님\n" +
+                "아이북에서 알림 신청한 '" + bookTitle + "' 도서의 최저가격이 갱신되었습니다.\n" +
+                "아이북에 방문하여 확인해주세요."); // 알림톡 내용
+        body.add("button_1", buttonInfo); // 버튼 정보
+        body.add("testMode", "N"); // 테스트 모드
+
+        //전송 객체 생성
+        HttpEntity<MultiValueMap<String, Object>> LowestPriceRequest = new HttpEntity<>(body);
+
+        //post로 보내고 결과 받기
+        ResponseEntity<String> LowestPriceResponse = rt.exchange(
+                "https://kakaoapi.aligo.in/akv10/alimtalk/send/",
+                HttpMethod.POST,
+                LowestPriceRequest,
+                String.class
+        );
+
+        return LowestPriceResponse;
+    }
+
 }
