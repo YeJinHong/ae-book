bf4c3ad34b170aea7eccd305fbc63ccb9fb1347f
leedoyeon <ldy__0202@naver.com>
2023-05-09 12:08:07 +0900

    feat: 동화책 목록
    S08P31C201-335
    
    
    Former-commit-id: fb84756ec14c8eb9361dce9bdac7fa5fa5261a5d

--- frontend/src/components/story/StoryListView2.vue
+++ frontend/src/components/story/StoryListView2.vue
@@ -3,18 +3,17 @@
     <h1>동화책 목록</h1>
     <router-link to="/story/keyword"><button class="ae-btn btn-red">동화 만들러 가기</button></router-link>
     <ModalView :modalShow="isModalVisible" @close-modal="closeModal">
-      <story-detail-view/>
-      <story-modal-button @close="closeModal"></story-modal-button>
+      <StoryDetailView />
     </ModalView>
     <div v-if="isLoginUser === false" class="story-container">
       로그인한 유저만 확인 가능합니다.
     </div>
     <div v-else class="story-container">
-      <div v-for="story in storyList" :key="story.id" @click="showModal(painting.id)">
+      <div v-for="story in storyList" :key="story.storyId" @click="showModal(story.storyId)">
         <list-item
-            :item="story"
-          >
-        </list-item>
+        :item="story"
+      >
+    </list-item>
       </div>
     </div>
     <div class="pagination-container">
@@ -24,72 +23,64 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import ModalView from '@/components/common/ModalView'
+import StoryDetailView from '@/components/story/StoryDetailView'
+import { mapMutations, mapGetters, mapState, mapActions } from 'vuex'
 import ListItem from '../common/list/ListItem.vue'
 import Pagination from '../common/Pagination.vue'
-import StoryModalButton from '../story/StoryModalButton.vue'
-import StoryDetailView from '@/components/story/StoryDetailView.vue'
-import ModalView from '@/components/common/ModalView.vue'
+
 const storyStore = 'storyStore'
 
 export default {
-  name: 'StoryListView2',
+  name: 'StoryListView',
+  components: {
+    ModalView,
+    StoryDetailView,
+    ListItem,
+    Pagination
+  },
   data () {
     return {
-      request: null,
-      isModalVisible: false,
-      isLoginUser: false
+      request: {
+        page: 0
+      },
+      isModalVisible: false
     }
-  },
-  components: {
-    ListItem,
-    Pagination,
-    StoryModalButton,
-    StoryDetailView,
-    ModalView
-  },
-  computed: {
-    ...mapState(storyStore, ['storyList', 'storyPageSetting'])
   },
   mounted () {
     if (sessionStorage.getItem('isLoginUser') === 'true') {
       this.isLoginUser = true
-      this.getStoryList()
+      this.getStoryList(this.request)
     }
   },
+  computed: {
+    ...mapState(storyStore, ['storyList', 'storyPageSetting']),
+    ...mapGetters(storyStore, ['getStoryId'])
+  },
   methods: {
-    ...mapActions(storyStore, ['getStoryList', 'getStoryDetail']),
-    onClickList (type) {
-      this.request = {
-        type: type
-      }
-      this.getStoryList(this.request)
-    },
-    paging (page) {
-      this.request['page'] = page - 1
-      this.getStoryList(this.request)
-    },
-    showModal (paintingId) {
-      this.getStoryDetail(paintingId)
+    ...mapActions(storyStore, ['getStoryList']),
+    ...mapMutations(storyStore, ['setStoryId', 'clearStoryId']),
+    showModal (storyId) {
+      this.setStoryId(storyId)
       this.isModalVisible = true
     },
     closeModal () {
       this.isModalVisible = false
+      this.clearStoryId()
+    },
+    paging (page) {
+      this.request['page'] = page - 1
+      this.getStoryList(this.request)
     }
   }
 }
 </script>
 
 <style scoped>
-
-h1 {
-  font-weight: 800;
-  margin: 10px 0px;
-}
 .story-container{
   margin: auto;
   margin-top: 30px;
-  width: 1000px;
+  width: 800px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -102,5 +93,10 @@ h1 {
 
 .disabled{
   background-color: lightgray;
+}
+
+h1 {
+  font-weight: 800;
+  margin: 10px 0px;
 }
 </style>
