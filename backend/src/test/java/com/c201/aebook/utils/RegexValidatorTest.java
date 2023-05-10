1ccd5ac4bbb614dbfbf4ac9a0983cc865dcc19c6
이동준 <dj0505.lee@samsung.com>
2023-05-10 23:09:43 +0900

    [S08P31C201] 단위 테스트 베이스 정리
    - 컨트롤러, 서비스, 유효성 검사기에 한해 단위 테스트를 작성하도록 유도
    - 단위 테스트 베이스 코드 정리해서 추가함
    
    Former-commit-id: d4f01c7ae5dc392d02df323fc70ff3336ded691e

---
+++ backend/src/test/java/com/c201/aebook/utils/RegexValidatorTest.java
@@ -0,0 +1,20 @@
+package com.c201.aebook.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RegexValidatorTest {
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@Test
+	public void testValidateIsbn() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
