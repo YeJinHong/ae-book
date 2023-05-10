fa4b3d6616a2795b11cd0c40b1e103eb349f756d
이동준 <dj0505.lee@samsung.com>
2023-05-10 23:09:43 +0900

    [S08P31C201] 단위 테스트 베이스 정리
    - 컨트롤러, 서비스, 유효성 검사기에 한해 단위 테스트를 작성하도록 유도
    - 단위 테스트 베이스 코드 정리해서 추가함
    
    Former-commit-id: d4f01c7ae5dc392d02df323fc70ff3336ded691e

---
+++ backend/src/test/java/com/c201/aebook/api/notification/presentation/controller/NotificationControllerTest.java
@@ -0,0 +1,40 @@
+package com.c201.aebook.api.notification.presentation.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class NotificationControllerTest {
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSaveNotification() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetNotificationBookList() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetNotificationBookDetail() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testUpdateNotification() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testDeleteNotification() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
---
+++ backend/src/test/java/com/c201/aebook/api/painting/service/impl/PaintingServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.c201.aebook.api.painting.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PaintingServiceImplTest {
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSavePainting() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetPaintingList() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testDeletePainting() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetFilePath() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePaintingTitle() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetPaintingDetails() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetNewPaintingList() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetOwnPainting() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
