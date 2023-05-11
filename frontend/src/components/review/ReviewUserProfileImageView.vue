b1f9df79c24e61431d33ab86cf4facdfadfa5990
문희주 <m5nacar5n@gmail.com>
2023-05-11 15:58:22 +0900

    fix: 리뷰 유저 프로필 사진 컴포넌트화 ..
    하지만 컴포넌트 설정은 덜됏음ㅋ
    S08P31C201-657
    
    
    Former-commit-id: a97d9dbe606ea865658dc00c7ca78d30ea4dd73a

---
+++ frontend/src/components/review/ReviewUserProfileImageView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div >
+    <img class="user-img" :src=imgUrl />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReviewUserProfileImageView',
+  props: {
+    imgUrl: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-img {
+  width: 50px;
+  height: 50px;
+  border-radius: 30px;
+  margin-right: 20px;
+}
+</style>
