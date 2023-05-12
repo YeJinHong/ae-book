e9e7a27b08222e7a0e3f3aa121554a9c2187b1f7
한선영 <gyeoul98@naver.com>
2023-05-12 14:15:58 +0900

    fix : 사용자 탈퇴 버튼 메뉴바로 이동
    사용자 수정 페이지 안에 있던 탈퇴 버튼을 메뉴바로 이동하였습니다.
    S08P31C201-713
    
    
    Former-commit-id: b404b2d73f69d5f4a4570d9a1a2e3ef31d64bed0

--- frontend/src/components/myPage/MyInfoModify.vue
+++ frontend/src/components/myPage/MyInfoModify.vue
@@ -31,40 +31,10 @@
           >
             수정
           </button>
-          <button type="button" class="ae-btn" v-b-modal.modal-delete-user>사용자 탈퇴</button>
         </div>
       </form>
 
     </div>
-
-    <b-modal
-      id="modal-delete-user"
-      ref="modal"
-      title="회원 탈퇴"
-      @show="resetModal"
-      @hidden="resetModal"
-      @ok="handleOk"
-      ok-title="확인"
-      ok-variant="danger"
-      cancel-title="취소"
-    >
-      <form ref="form" @submit.stop.prevent="handleSubmit">
-        <b-form-group
-          label="회원 탈퇴를 원하신다면 아래의 문구를 입력해주세요."
-          label-for="msg-input"
-          invalid-feedback="문구를 올바르게 입력해주세요"
-          :state="msgState"
-        >
-          <h6>&lt; 회원 탈퇴를 진행합니다 &gt;</h6><br>
-          <b-form-input
-            id="msg-input"
-            v-model="msg"
-            :state="msgState"
-            required
-          ></b-form-input>
-        </b-form-group>
-      </form>
-    </b-modal>
 
   </div>
 </template>
@@ -90,7 +60,6 @@ export default {
     } else {
       const userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
       this.user.nickname = userInfo.nickname
-      // this.user = userInfo
     }
   },
   computed: {
@@ -119,51 +88,6 @@ export default {
 
       await this.userUpdate(formData)
       alert('수정이 완료되었습니다.')
-    },
-    deleteUserInfo () {
-      console.log('사용자 탈퇴')
-      if (confirm('정말로 탈퇴하시겠습니까?')) {
-        this.userDelete()
-          .then(() => {
-            return this.userLogout()
-          })
-          .then(() => {
-            alert('그동안 서비스를 이용해주셔서 감사합니다')
-            this.$router.push({ name: 'Main' })
-          })
-          .catch((error) => {
-            console.error(error)
-          })
-      }
-    },
-    checkFormValidity () {
-      const valid = this.$refs.form.checkValidity()
-      this.msgState = valid
-      return valid
-    },
-    resetModal () {
-      this.msg = ''
-      this.msgState = null
-    },
-    handleOk (bvModalEvent) {
-      bvModalEvent.preventDefault()
-      this.handleSubmit()
-    },
-    handleSubmit () {
-      // Exit when the form isn't valid
-      if (!this.checkFormValidity()) {
-        return
-      }
-
-      if (this.msg === '회원 탈퇴를 진행합니다') {
-        this.deleteUserInfo()
-        this.$nextTick(() => {
-          this.$bvModal.hide('modal-delete-user')
-        })
-      } else {
-        this.msgState = false
-        this.$refs.form.reportValidity()
-      }
     }
   }
 
