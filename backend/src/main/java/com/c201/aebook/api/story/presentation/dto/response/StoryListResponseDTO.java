fa67bd09551e66c5a89dff27a598e6266e9e8561
홍예진 <teepij@naver.com>
2023-05-15 11:57:46 +0900

    feat: 동화 리스트, 상세 조회 DTO 분리
    리스트 조회시 필요없는 정보가 있어 별도 분리
    S08P31C201-559
    
    
    Former-commit-id: 4ec2b16fad0a7c03729abf1c2dc857d23b64baec

--- backend/src/main/java/com/c201/aebook/api/story/presentation/dto/response/StoryListResponseDTO.java
+++ backend/src/main/java/com/c201/aebook/api/story/presentation/dto/response/StoryListResponseDTO.java
@@ -11,24 +11,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class StoryResponseDTO {
+public class StoryListResponseDTO {
+
 	@Schema(description = "동화 ID", defaultValue = "1")
 	private Long storyId;
 
 	@Schema(description = "동화 작성자 nickname", defaultValue = "동글이")
-	private String storyAuthorNickname;
+	private String nickname;
 
 	@Schema(description = "동화 제목", defaultValue = "아기 공룡의 하루")
 	private String title;
 
-	@Schema(description = "동화 내용", defaultValue = "아기 공룡은 밥을 먹기 싫어했어요. 그치만 참고 밥을 많이 먹었더니 몸이 튼튼해지고 강해졌어요. 아기 공룡은 튼튼한 어른 공룡이 되었답니다.")
-	private String content;
-
 	@Schema(description = "동화 이미지 url", defaultValue = "https://aebookbucket.s3.ap-northeast-2.amazonaws.com/%EC%95%84%EC%9D%B4%EB%B6%81.png")
 	private String imgUrl;
-
-	@Schema(description = "동화 보이스 url")
-	private String voiceUrl;
 
 	@Schema(description = "동화 작성 시간", defaultValue = "2023-04-14 10:30:15")
 	private LocalDateTime createdAt;
@@ -37,14 +32,12 @@ public class StoryResponseDTO {
 	private LocalDateTime updatedAt;
 
 	@Builder
-	public StoryResponseDTO(Long storyId, String storyAuthorNickname, String title, String content, String imgUrl,
-		String voiceUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
+	public StoryListResponseDTO(Long storyId, String nickname, String title, String imgUrl,
+		LocalDateTime createdAt, LocalDateTime updatedAt) {
 		this.storyId = storyId;
-		this.storyAuthorNickname = storyAuthorNickname;
+		this.nickname = nickname;
 		this.title = title;
-		this.content = content;
 		this.imgUrl = imgUrl;
-		this.voiceUrl = voiceUrl;
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
