6ebf549e495a764f06659d1898ee2c037726e62d
문희주 <m5nacar5n@gmail.com>
2023-05-15 11:06:19 +0900

    feat: RegexValidatorTest 작성 중 ..
    의문 생겨서 잠시 멈추고 다른 일 ..
    S08P31C201-555
    
    
    Former-commit-id: cbbd4c465235afb7425d689aac8dbabe08e07e47

--- backend/src/test/java/com/c201/aebook/utils/RegexValidatorTest.java
+++ backend/src/test/java/com/c201/aebook/utils/RegexValidatorTest.java
@@ -1,20 +1,65 @@
 package com.c201.aebook.utils;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class RegexValidatorTest {
+	@InjectMocks
+	private RegexValidator regexValidator;
+
+	@BeforeAll
+	protected static void init() {
+		System.out.println("=== RegexValidator Test Init ===");
+	}
+
+	@AfterAll
+	protected static void close() {
+		System.out.println("=== RegexValidator Test Close ===");
+	}
 
 	@BeforeEach
 	protected void setUp() throws Exception {
+		System.out.println("=== Start ===");
+	}
+
+	@AfterEach
+	protected void finish() throws Exception {
+		System.out.println("=== Finish ===");
 	}
 
 	@Test
-	public void testValidateIsbn() {
-		throw new RuntimeException("not yet implemented");
+	@DisplayName("testValidateIsbn: Happy Case 10")
+	public void testValidateIsbn1() {
+		// throw new RuntimeException("not yet implemented");
+
+		// given
+		String isbn = "A123456789";
+
+		// when
+
+		// then
+	}
+
+	@Test
+	@DisplayName("testValidateIsbn: Happy Case 13")
+	public void testValidateIsbn2() {
+		// throw new RuntimeException("not yet implemented");
+
+		// given
+		String isbn = "1234567891011";
+
+		// when
+		regexValidator.validateIsbn(isbn);
+
+		// then
 	}
 
 }
