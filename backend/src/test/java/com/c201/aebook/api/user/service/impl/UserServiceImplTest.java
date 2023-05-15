40a8ba08dbb34585bc25b0313f712e4c5a085443
한선영 <gyeoul98@naver.com>
2023-05-15 23:49:22 +0900

    test : testDuplicatedUserByNickname 테스트 코드 작성
    testDuplicatedUserByNickname 테스트 코드를 작성하였습니다. 우선 happy case 먼저 작성하였습니다.
    S08P31C201-815
    
    
    Former-commit-id: 64c31f8f39bdfb08ce3d07bd47087e86a9a4b99e

--- backend/src/test/java/com/c201/aebook/api/user/service/impl/UserServiceImplTest.java
+++ backend/src/test/java/com/c201/aebook/api/user/service/impl/UserServiceImplTest.java
@@ -1,20 +1,45 @@
 package com.c201.aebook.api.user.service.impl;
 
+import com.c201.aebook.api.user.persistence.repository.UserRepository;
+import com.c201.aebook.converter.UserConverter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
+
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private UserConverter userConverter;
+
+	@InjectMocks
+	private UserServiceImpl subject;
 
 	@BeforeEach
 	protected void setUp() throws Exception {
 	}
 
 	@Test
+	@DisplayName("testDuplicatedUserByNickname: Happy Case")
 	public void testDuplicatedUserByNickname() {
-		throw new RuntimeException("not yet implemented");
+		// given
+		String nickname = "test nickname";
+		Boolean checkNickname = false;
+
+		BDDMockito.given(userRepository.existsByNickname(nickname)).willReturn(checkNickname);
+
+		// when
+		subject.duplicatedUserByNickname(nickname);
+
+		// then
+
 	}
 
 	@Test
