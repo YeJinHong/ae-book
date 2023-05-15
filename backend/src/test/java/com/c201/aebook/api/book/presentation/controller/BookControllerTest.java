5a2049cb83a63a7b7b3d36621da0e947e730000b
leedoyeon <ldy__0202@naver.com>
2023-05-15 14:00:27 +0900

    test: testSearchBookDetail 작성
    S08P31C201-727
    
    
    Former-commit-id: 968a7f5b9e3e1fd5166cb973cdbbe436d4f2069e

--- backend/src/test/java/com/c201/aebook/api/book/presentation/controller/BookControllerTest.java
+++ backend/src/test/java/com/c201/aebook/api/book/presentation/controller/BookControllerTest.java
@@ -1,20 +1,53 @@
 package com.c201.aebook.api.book.presentation.controller;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.c201.aebook.api.book.presentation.dto.response.BookResponseDTO;
+import com.c201.aebook.api.book.service.impl.BookServiceImpl;
+import com.c201.aebook.api.user.persistence.entity.UserEntity;
+import com.c201.aebook.auth.CustomUserDetails;
+import com.c201.aebook.utils.RegexValidator;
 
 @ExtendWith(SpringExtension.class)
+@Import(BookController.class)
 public class BookControllerTest {
+	@Autowired
+	private BookController bookController;
+	private MockMvc mockMvc;
+	@MockBean
+	private BookServiceImpl bookService;
+	@MockBean
+	private RegexValidator regexValidator;
 
 	@BeforeEach
 	protected void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
 	}
 
 	@Test
-	public void testSearchBookDetail() {
-		throw new RuntimeException("not yet implemented");
+	public void testSearchBookDetail() throws Exception {
+		// given
+		String isbn = "9791185121147";
+		CustomUserDetails customUserDetails = new CustomUserDetails(UserEntity.builder().id(1L).build());
+		BookResponseDTO responseDTO = BookResponseDTO.builder().isbn(isbn).build();
+		BDDMockito.given(bookService.searchBookDetail(isbn, customUserDetails)).willReturn(responseDTO);
+		// when
+		mockMvc.perform(get("/books/" + isbn)).andExpect(status().isOk());
+		// then
+		BDDMockito.verify(bookService).searchBookDetail(eq(isbn), any(CustomUserDetails.class));
 	}
 
 	@Test
