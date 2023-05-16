e14a28313070dde72c334ba8b61d3456dee37c05
이해민 <2haemin98@gmail.com>
2023-05-16 10:26:47 +0900

    comment : 불필요한  주석 제거
    주석 제거
    S08P31C201-763
    
    
    Former-commit-id: ee46bc858684d2ba814080b005ca2bac83faa17c

--- batch/src/test/java/com/c201/aebook/api/batch/AladinBatchItemReaderTest.java
+++ batch/src/test/java/com/c201/aebook/api/batch/AladinBatchItemReaderTest.java
@@ -1,14 +1,17 @@
 package com.c201.aebook.api.batch;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,6 +23,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.c201.aebook.api.book.persistence.entity.BookEntity;
 
@@ -42,6 +46,7 @@ public class AladinBatchItemReaderTest {
 		// given
 		AladinBatchItemReader mockReader = Mockito.spy(subject);
 		List<BookEntity> mockBooks = new ArrayList<>();
+
 		mockBooks.add(BookEntity.builder().author("author").title("title").build());
 		Mockito.doReturn(mockBooks).when(mockReader).getDataFromApi();
 
@@ -49,46 +54,13 @@ public class AladinBatchItemReaderTest {
 		BookEntity actualBook = mockReader.read();
 
 		// then
-		assertNotNull(actualBook);
+		Assertions.assertNotNull(actualBook);
 	}
 
-	// @Test
-	// @DisplayName("happy case")
-	// public void testRead() throws Exception {
-	// 	//given
-	// 	List<BookEntity> bookList = new ArrayList<>();
-	// 	//bookList.add(BookEntity.builder().author("author1").title("title1").build());
-	// 	//bookList.add(BookEntity.builder().author("author2").title("title2").build());
-	//
-	// 	// restTemplate mock 설정
-	// 	Mockito.when(restTemplate.getForObject(anyString(), eq(String.class))).thenReturn("dummy response");
-	//
-	// 	// subject.getDataFromApi() mock 설정
-	// 	AladinBatchItemReader mockReader = Mockito.spy(subject);
-	// 	NodeList mockNodeList = mock(NodeList.class);
-	//
-	// 	doReturn(mockNodeList).when(mockReader).getItemElementByUrl(any(UriComponentsBuilder.class), eq("item"));
-	// 	when(mockNodeList.getLength()).thenReturn(bookList.size()); // getLength() 메소드 수정
-	// 	when(mockReader.parseBook(any(Node.class))).thenReturn(BookEntity.builder().author("author").title("title").build());
-	// 	when(mockReader.getDataFromApi()).thenReturn(bookList);
-	//
-	// 	// when
-	// 	BookEntity actualBook = mockReader.read();
-	//
-	// 	// then
-	// 	assertNotNull(actualBook);
-	// 	//assertEquals("title", actualBook.getTitle());
-	// 	//assertEquals("author", actualBook.getAuthor());
-	// }
 
 	@Test
-	@DisplayName("happy case")
-	public void testGetDataFromApi() {
-		//given
+	public void testGetDataFromApi() throws IOException, ParserConfigurationException, SAXException, ParseException {
 
-		//when
-
-		//then
 
 	}
 
