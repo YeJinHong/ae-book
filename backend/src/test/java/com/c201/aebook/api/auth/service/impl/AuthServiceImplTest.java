5f16656fcaad9d46e15fff338e49586357a8efdd
한선영 <gyeoul98@naver.com>
2023-05-18 01:41:37 +0900

    test : testSaveUserOrLogin 테스트 코드 작성
    testSaveUserOrLogin 테스트 코드 작성하였습니다.
    S08P31C201-
    
    
    Former-commit-id: aec40277fa41e600db2a8bbb74da205dd444b797

--- backend/src/test/java/com/c201/aebook/api/auth/service/impl/AuthServiceImplTest.java
+++ backend/src/test/java/com/c201/aebook/api/auth/service/impl/AuthServiceImplTest.java
@@ -1,5 +1,9 @@
 package com.c201.aebook.api.auth.service.impl;
 
+import com.c201.aebook.api.common.LoginUserInfoDTO;
+import com.c201.aebook.api.common.TokenDTO;
+import com.c201.aebook.api.user.persistence.entity.RefreshRedisTokenEntity;
+import com.c201.aebook.api.user.persistence.entity.UserEntity;
 import com.c201.aebook.api.user.persistence.repository.RefreshRedisRepository;
 import com.c201.aebook.api.user.persistence.repository.UserRepository;
 import com.c201.aebook.auth.dto.KakaoTokenDTO;
@@ -7,15 +11,13 @@ import com.c201.aebook.auth.profile.KakaoProfile;
 import com.c201.aebook.config.jwt.JwtTokenProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.internal.parser.Token;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.*;
@@ -26,8 +28,8 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 
@@ -55,7 +57,8 @@ public class AuthServiceImplTest {
 	
 	@Mock
 	private ObjectMapper objectMapper;
-	
+
+	@Spy
 	@InjectMocks
 	private AuthServiceImpl subject;
 
@@ -147,8 +150,59 @@ public class AuthServiceImplTest {
 	}
 
 	@Test
+	@DisplayName("testSaveUserOrLogin: Happy Case")
 	public void testSaveUserOrLogin() {
-		throw new RuntimeException("not yet implemented");
+		String token = "test kakao access token";
+		String kakaoProfileJson = "{\"id\":123456789,\"connected_at\":\"2023-04-19T02:22:56Z\",\"properties\":{\"nickname\":\"test nickname\",\"profile_image\":\"http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg\",\"thumbnail_image\":\"http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg\"},\"kakao_account\":{\"profile_nickname_needs_agreement\":false,\"profile_image_needs_agreement\":false,\"profile\":{\"nickname\":\"test nickname\",\"thumbnail_image_url\":\"http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg\",\"profile_image_url\":\"http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg\",\"is_default_image\":true},\"has_phone_number\":true,\"phone_number_needs_agreement\":false,\"phone_number\":\"+82 10-0000-0000\"}}";
+		Long kakaoId = 123456789L;
+		KakaoProfile.KakaoAccount.Profile profile = KakaoProfile.KakaoAccount.Profile.builder()
+				.nickname("test nickname")
+				.thumbnail_image_url("thumbnail img")
+				.profile_image_url("profile img")
+				.is_default_image(true).build();
+		KakaoProfile.KakaoAccount kakaoAccount = KakaoProfile.KakaoAccount.builder()
+				.profile_nickname_needs_agreement(false)
+				.profile_image_needs_agreement(false)
+				.profile(profile)
+				.has_phone_number(true)
+				.phone_number_needs_agreement(false)
+				.phone_number("+82 10-0000-0000").build();
+		KakaoProfile.Properties properties = KakaoProfile.Properties.builder()
+				.nickname("test nickname")
+				.profile_image("profile img")
+				.thumbnail_image("thumbnail img").build();
+		KakaoProfile kakaoProfile = KakaoProfile.builder().id(kakaoId).connected_at("2023-04-19T02:22:56Z").kakao_account(kakaoAccount).properties(properties).build();
+
+		BDDMockito.given(restTemplate.exchange(
+				eq("https://kapi.kakao.com/v2/user/me"),
+				eq(HttpMethod.POST),
+				any(HttpEntity.class),
+				eq(String.class)
+		)).willReturn(ResponseEntity.ok().body(kakaoProfileJson));
+
+		BDDMockito.given(userRepository.findByKakaoId(any())).willReturn(null);
+		BDDMockito.given(userRepository.countByNicknameStartingWith(any())).willReturn(1L);
+
+		TokenDTO tokenDTO = TokenDTO.builder().accessToken("access token").refreshToken("refresh token").grantType("type").AuthorizationHeader("header").accessTokenExpiresIn(1234L).build();
+		BDDMockito.given(jwtTokenProvider.generateTokenDto(anyString())).willReturn(tokenDTO);
+
+		RefreshRedisTokenEntity refreshRedisToken = RefreshRedisTokenEntity.createToken(anyString(), tokenDTO.getRefreshToken());
+
+		// when
+		LoginUserInfoDTO ret = subject.saveUserOrLogin(token);
+
+		// then
+		Assertions.assertAll("결괏값 검증", () -> {
+			Assertions.assertNotNull(ret);
+			Assertions.assertEquals(ret.getUser().getNickname(), "test nickname2");
+			Assertions.assertEquals(ret.getUser().getKakaoId(), 123456789L);
+			Assertions.assertEquals(ret.getUser().getPhone(), "+82 10-0000-0000");
+			Assertions.assertEquals(ret.getUser().getProfileUrl(), "http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg");
+			Assertions.assertEquals(ret.getUser().getStatus(), 1);
+			Assertions.assertEquals(ret.getTokenDto().getAccessToken(), "access token");
+			Assertions.assertEquals(ret.getTokenDto().getRefreshToken(), "refresh token");
+		});
+
 	}
 
 	@Test
