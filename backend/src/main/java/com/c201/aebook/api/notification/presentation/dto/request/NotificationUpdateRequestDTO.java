0696f23b5ceef95cb728830b09d555771db2bb96
한선영 <gyeoul98@naver.com>
2023-05-18 13:15:02 +0900

    feat : NotificationUpdateRequestDTO @Builder 어노테이션 추가
    NotificationUpdateRequestDTO @Builder 어노테이션을 추가하였습니다.
    S08P31C201-804
    
    
    Former-commit-id: 4a7b240c74fbd1f02b2365d0fa16797e52e12570

--- backend/src/main/java/com/c201/aebook/api/notification/presentation/dto/request/NotificationUpdateRequestDTO.java
+++ backend/src/main/java/com/c201/aebook/api/notification/presentation/dto/request/NotificationUpdateRequestDTO.java
@@ -1,15 +1,13 @@
 package com.c201.aebook.api.notification.presentation.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class NotificationUpdateRequestDTO {
 
     @Schema(description = "금액 상한선", defaultValue = "5000")
