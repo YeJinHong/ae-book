177ccc5c144001746fba9e80c509cb80a0e3d3ef
YeJinHong <teepij@naver.com>
2023-05-19 01:40:00 +0900

    fix: getMyStoryList 수정
    findByUserId 대신 findAll을 쓰려다 pageable을 잘못사용했습니다.
    입력받은 페이지 크기 안에서 해당하는 유저를 필터링하게 되어 항상 3개
    이하가 출력되었습니다.
    게다가 findAll를 사용하는 구조는 pageable의 sort가 반영되지
    않습니다. 방법은 있겠지만 주객이 전도되는 느낌이 있어 findAll 사용을
    포기합니다...
    S08P31C201-
    
    
    Former-commit-id: f5c499ec1945c2bc1b2be43fe39120b3ca274281

--- backend/src/main/java/com/c201/aebook/api/story/persistence/repository/StoryRepository.java
+++ backend/src/main/java/com/c201/aebook/api/story/persistence/repository/StoryRepository.java
@@ -1,5 +1,6 @@
 package com.c201.aebook.api.story.persistence.repository;
 
+import com.c201.aebook.api.review.persistence.entity.ReviewEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import com.c201.aebook.api.story.persistence.entity.StoryEntity;
 
 @Repository
 public interface StoryRepository extends JpaRepository<StoryEntity, Long> {
-
+    @Query(value = "SELECT se FROM StoryEntity se LEFT JOIN FETCH se.user WHERE se.user.id = :userId",
+            countQuery = "SELECT COUNT(se) FROM StoryEntity se WHERE se.user.id = :userId")
+    Page<StoryEntity> findByUserId(Long userId, Pageable pageable);
 }
