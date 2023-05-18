d55b4e68dd2ee2c01cf10e2a623b8e25e0db1def
yundaehyuck <dbseogur2000@naver.com>
2023-05-18 15:28:35 +0900

    comment: delete console log
    Former-commit-id: 824d03521ccb13e7e3f96248857fe06290666c5f

--- frontend/src/components/review/ReviewMyItemView.vue
+++ frontend/src/components/review/ReviewMyItemView.vue
@@ -132,8 +132,6 @@ export default {
         await this.modifyReviewAction(payload)
         await this.$emit('paging', this.page + 1)
 
-        console.log(this.book)
-
         this.book.scoreSum += this.updateScore - this.review.score
         // 2. 수정 반영해서 리스트 가져오기 : emit 완료보다 상태변경이 빨라서 딜레이 설정
         setTimeout(() => {
@@ -161,9 +159,7 @@ export default {
     async deleteReview () {
       if (confirm('삭제하시겠습니까?')) {
         await this.deleteReviewAction(this.review.id)
-        console.log(this.review.isbn)
         this.getBookDetail(this.review.isbn)
-        console.log(this.book)
         this.book.scoreSum -= this.review.score
         this.book.reviewCount -= 1
       }
